3ddce2ad8a7d3043780adf9f538ec4565756af99
Corban Riley <corbanbrook@gmail.com>
2022-10-25 12:11:34 -0400

    Adding Tabs component

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,92 @@
+import * as TabsPrimitive from '@radix-ui/react-tabs'
+import type { TabsProps as BaseTabsProps } from '@radix-ui/react-tabs'
+import { ReactNode, useState } from 'react'
+
+import * as styles from './styles.css'
+
+interface TabsProps extends BaseTabsProps {
+  tabs: TabItemProps[]
+}
+
+interface TabItemProps {
+  value: string
+  title: string
+  content: ReactNode
+}
+
+export const Tabs = (props: TabsProps) => {
+  const { tabs, onValueChange, ...rest } = props
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(rest.defaultValue)
+  if (!tabs.length) {
+    return null
+  }
+
+  const selectorWidth = 100 / tabs.length
+  const selectedIdx = tabs.findIndex(tab => tab.value === selectedValue)
+
+  const handleValueChange = (value: string) => {
+    setSelectedValue(value)
+    onValueChange?.(value)
+  }
+
+  return (
+    <TabsPrimitive.Root onValueChange={handleValueChange} {...rest}>
+      <TabsPrimitive.List className={styles.list}>
+        <div className={styles.selectorContainer}>
+          <div
+            className={styles.selector}
+            style={{ width: `${selectorWidth}%`, transform: `translateX(${selectedIdx * 100}%)` }}
+          />
+        </div>
+        {tabs.map(tab => (
+          <TabsPrimitive.Trigger className={styles.trigger} key={tab.value} value={tab.value}>
+            {tab.title}
+          </TabsPrimitive.Trigger>
+        ))}
+      </TabsPrimitive.List>
+      {tabs.map(tab => (
+        <TabsPrimitive.Content key={tab.value} value={tab.value}>
+          {tab.content}
+        </TabsPrimitive.Content>
+      ))}
+    </TabsPrimitive.Root>
+  )
+}
+
+// export const TabsRoot = TabsPrimitive.Root
+
+// export const TabsContent = TabsPrimitive.TabsContent
+
+// export const TabsTrigger = forwardRef<
+//   React.ElementRef<typeof TabsPrimitive.TabsTrigger>,
+//   React.ComponentPropsWithoutRef<typeof TabsPrimitive.TabsTrigger>
+// >((props, forwardedRef) => {
+//   const { className, ...rest } = props
+//   return <TabsPrimitive.TabsTrigger className={clsx(className, styles.trigger)} {...rest} ref={forwardedRef} />
+// })
+// TabsTrigger.displayName = 'TabsTrigger'
+
+// export const TabsList = (props: PropsWithChildren<{}>) => {
+//   const { children } = props
+
+//   const selectorWidth = 100 / Children.count(children)
+
+//   const selectedIdx = Children.toArray(children).findIndex(child => {
+//     if (!isValidElement(child)) {
+//       return
+//     }
+
+//     const { value } = child.props
+
+//     console.log('child props', child.props)
+//   })
+
+//   return (
+//     <TabsPrimitive.TabsList className={styles.list} >
+//       <div className={styles.selectorContainer}>
+//         <div className={styles.selector} style={{ width: `${selectorWidth}%`, transform: `translateX(${selectedIdx * 100}%)` }} />
+//       </div>
+//       {children}
+//     </TabsPrimitive.TabsList>
+//   )
+// }
---
+++ src/components/Tabs/styles.css.ts
@@ -0,0 +1,64 @@
+import { style } from '@vanilla-extract/css'
+
+import { vars } from '../../css'
+import { typography } from '../../css/typography.css'
+
+export const trigger = style([
+  typography.h2,
+  {
+    appearance: 'none',
+    border: 'none',
+    background: 'none',
+    height: '100%',
+    width: '50%',
+    position: 'relative',
+    zIndex: '2',
+    cursor: 'pointer',
+    userSelect: 'none',
+
+    color: vars.colors.scale10,
+
+    selectors: {
+      '&[data-state="active"]': {
+        color: vars.colors.scale12
+      },
+      '&[disabled]': {
+        opacity: 0.3,
+        pointerEvents: 'none'
+      }
+    }
+  }
+])
+
+export const list = style({
+  background: vars.colors.scale3,
+  width: '100%',
+  height: '50px',
+  borderRadius: vars.radii.md,
+  position: 'relative'
+})
+
+export const selectorContainer = style({
+  height: '32px',
+  display: 'flex',
+  top: '9px',
+  bottom: '9px',
+  left: '15px',
+  right: '15px',
+  position: 'absolute'
+})
+
+export const selector = style({
+  background: vars.colors.scale5,
+  position: 'absolute',
+  width: '50%',
+  height: '100%',
+  top: '0',
+  left: '0',
+
+  zIndex: '1',
+
+  borderRadius: vars.radii.md,
+
+  transition: 'transform 200ms ease-out'
+})
