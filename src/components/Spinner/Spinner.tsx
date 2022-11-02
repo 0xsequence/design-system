2c2c5e457c4cac9250e002b2c5e6e0708e4f12e6
Corban Riley <corbanbrook@gmail.com>
2022-11-02 18:11:39 -0400

    Fixing spinner ref

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react'
+import { forwardRef, Ref } from 'react'
 
 import { Box } from '~/components/Box'
 
@@ -8,30 +8,33 @@ type SpinnerProps = {
   // color?: BoxProps['color']
 } & styles.Variants
 
-export const Spinner = forwardRef<HTMLElement, SpinnerProps>((props, ref) => {
-  const { size = 'md' } = props
-  return (
-    <Box className={styles.variants({ size })} ref={ref}>
-      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <circle
-          cx="12"
-          cy="12"
-          fill="none"
-          r="10"
-          strokeDasharray="42"
-          strokeLinecap="round"
-        />
-        <circle
-          cx="12"
-          cy="12"
-          fill="none"
-          opacity="0.25"
-          r="10"
-          strokeLinecap="round"
-        />
-      </svg>
-    </Box>
-  )
-})
+export const Spinner = forwardRef(
+  (props: SpinnerProps, ref: Ref<HTMLDivElement>) => {
+    const { size = 'md' } = props
+
+    return (
+      <Box className={styles.variants({ size })} ref={ref}>
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <circle
+            cx="12"
+            cy="12"
+            fill="none"
+            r="10"
+            strokeDasharray="42"
+            strokeLinecap="round"
+          />
+          <circle
+            cx="12"
+            cy="12"
+            fill="none"
+            opacity="0.25"
+            r="10"
+            strokeLinecap="round"
+          />
+        </svg>
+      </Box>
+    )
+  }
+)
 
 Spinner.displayName = 'Spinner'
