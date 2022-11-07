7328b8d7c7bc7d5d94718cc24fc53bd703f7e017
Shawn Yeung <sy@horizon.io>
2022-11-07 15:28:34 -0500

    create file for types

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,85 +1,10 @@
-import {
-  AllHTMLAttributes,
-  ChangeEvent,
-  EventHandler,
-  forwardRef,
-  ReactNode,
-  Ref,
-} from 'react'
+import { forwardRef, Ref } from 'react'
 
-import { Box, BoxProps } from '~/components/Box'
+import { Box } from '~/components/Box'
 import { LabelledField } from '~/components/LabelledField'
 
 import * as styles from './styles.css'
-
-type InputProps = AllHTMLAttributes<HTMLInputElement>
-type TextAreaProps = AllHTMLAttributes<HTMLTextAreaElement>
-
-type AsText = {
-  type?: 'text'
-  maxLength?: InputProps['maxLength']
-}
-
-type AsEmail = {
-  type: 'email'
-}
-
-type AsNumber = {
-  type: 'number'
-  max?: InputProps['max']
-  min?: InputProps['min']
-  step?: InputProps['step']
-}
-
-type AsInput = {
-  as?: 'input'
-  onChange?: EventHandler<ChangeEvent<HTMLInputElement>>
-  leftIcon?: ReactNode
-  rightIcon?: ReactNode
-} & (AsText | AsEmail | AsNumber)
-
-// TODO: fix types to make textarea work
-type AsTextArea = {
-  as: 'textarea'
-  onChange?: EventHandler<ChangeEvent<HTMLTextAreaElement>>
-  cols?: TextAreaProps['cols']
-  rows?: TextAreaProps['rows']
-  type: never
-}
-
-type hiddenLabel = {
-  label?: string
-  labelLocation?: 'hidden'
-}
-
-type hasLabel = {
-  label: string
-  labelLocation: 'left' | 'top'
-}
-
-type TextInputProps<T extends 'input' | 'textarea' = 'input'> = Partial<
-  Pick<
-    BoxProps<T>,
-    | 'as'
-    | 'alignSelf'
-    | 'justifySelf'
-    | 'margin'
-    | 'marginBottom'
-    | 'marginLeft'
-    | 'marginRight'
-    | 'marginTop'
-    | 'marginX'
-    | 'marginY'
-  >
-> &
-  (AsInput | AsTextArea) &
-  (hasLabel | hiddenLabel) & {
-    name?: string
-    disabled?: boolean
-    processing?: boolean
-    placeholder?: string
-    value?: string
-  }
+import { TextInputProps } from './types'
 
 export const TextInput = forwardRef(
   <T extends 'input' | 'textarea'>(props: TextInputProps<T>, ref: Ref<T>) => {
