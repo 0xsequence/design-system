13ea5c23f3f3fef69a49b3b6ec166bc40a643d80
Shawn Yeung <sy@horizon.io>
2022-11-07 15:28:34 -0500

    create file for types

---
+++ src/components/TextInput/types.ts
@@ -0,0 +1,71 @@
+import { AllHTMLAttributes, ChangeEvent, EventHandler, ReactNode } from 'react'
+
+import { BoxProps } from '~/components/Box'
+
+type InputProps = AllHTMLAttributes<HTMLInputElement>
+type TextAreaProps = AllHTMLAttributes<HTMLTextAreaElement>
+
+type AsText = {
+  type?: 'text'
+  maxLength?: InputProps['maxLength']
+}
+
+type AsEmail = {
+  type: 'email'
+}
+
+type AsNumber = {
+  type: 'number'
+  max?: InputProps['max']
+  min?: InputProps['min']
+  step?: InputProps['step']
+}
+
+type AsInput = {
+  as?: 'input'
+  onChange?: EventHandler<ChangeEvent<HTMLInputElement>>
+  leftIcon?: ReactNode
+  rightIcon?: ReactNode
+} & (AsText | AsEmail | AsNumber)
+
+type AsTextArea = {
+  as: 'textarea'
+  onChange?: EventHandler<ChangeEvent<HTMLTextAreaElement>>
+  cols?: TextAreaProps['cols']
+  rows?: TextAreaProps['rows']
+  type: never
+}
+
+type hiddenLabel = {
+  label?: string
+  labelLocation?: 'hidden'
+}
+
+type hasLabel = {
+  label: string
+  labelLocation: 'left' | 'top'
+}
+
+export type TextInputProps<T extends 'input' | 'textarea' = 'input'> = Partial<
+  Pick<
+    BoxProps<T>,
+    | 'as'
+    | 'alignSelf'
+    | 'justifySelf'
+    | 'margin'
+    | 'marginBottom'
+    | 'marginLeft'
+    | 'marginRight'
+    | 'marginTop'
+    | 'marginX'
+    | 'marginY'
+  >
+> &
+  (AsInput | AsTextArea) &
+  (hasLabel | hiddenLabel) & {
+    name?: string
+    disabled?: boolean
+    processing?: boolean
+    placeholder?: string
+    value?: string
+  }
