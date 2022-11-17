4e10cc0ca90373d1aacbc9379da6e403355e2fb3
Shawn Yeung <sy@horizon.io>
2022-11-17 15:54:11 -0500

    test config

---
+++ docs/assets/Tabs.stories.cfa83586.js
@@ -0,0 +1,31 @@
+var s=Object.defineProperty;var o=(t,e)=>s(t,"name",{value:e,configurable:!0});import{T as n}from"./Tabs.e5ad6081.js";import{j as l}from"./jsx-runtime.91a17a4d.js";import"./index.module.6a94d229.js";import"./index.68078f7f.js";import"./iframe.68c65bad.js";import"./index.module.f82eba39.js";import"./Text.8d02ece9.js";import"./Box.f52b9a27.js";import"./atoms.css.9060149a.js";/* empty css                              *//* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm.e63f122f.js";const v={parameters:{storySource:{source:`import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Tabs } from './Tabs'
+
+export default {
+  title: 'Components/Tabs',
+  component: Tabs
+} as ComponentMeta<typeof Tabs>
+
+const Template: ComponentStory<typeof Tabs> = args => {
+  return <Tabs {...args} />
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  defaultValue: 'coins',
+  tabs: [
+    {
+      value: 'coins',
+      title: 'Coins',
+      content: 'Coins'
+    },
+    {
+      value: 'collectibles',
+      title: 'Collectibles',
+      content: 'Collectibles'
+    }
+  ]
+}
+`,locationsMap:{default:{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}}}}},title:"Components/Tabs",component:n},a=o(t=>l(n,{...t}),"Template"),r=a.bind({});r.args={defaultValue:"coins",tabs:[{value:"coins",title:"Coins",content:"Coins"},{value:"collectibles",title:"Collectibles",content:"Collectibles"}]};const D=["Default"];export{r as Default,D as __namedExportsOrder,v as default};
+//# sourceMappingURL=Tabs.stories.cfa83586.js.map
