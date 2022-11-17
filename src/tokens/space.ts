bc5d2ef82c71f9c18d4b61734b3305a9ffc9f53d
Corban Riley <corbanbrook@gmail.com>
2022-11-17 14:42:07 -0500

    final space tokens

--- src/tokens/space.ts
+++ src/tokens/space.ts
@@ -1,13 +1,14 @@
 // prettier-ignore
 export const space = {
   '0':    '0',          // 0px  none
-  // '0.25': '0.0625rem',  // 1px
-  // '0.5':  '0.125rem',   // 2px  
+  '0.25': '0.0625rem',  // 1px
+  '0.5':  '0.125rem',   // 2px
   '1':    '0.25rem',    // 4px  xxtight
+  '1.5':  '0.375rem',   // 6px
   '2':    '0.5rem',     // 8px  xtight
   '3':    '0.75rem',    // 12px tight
   '4':    '1rem',       // 16px normal
-  '5':    '1.25rem',    // 20px 
+  '5':    '1.25rem',    // 20px
   '6':    '1.5rem',     // 24px loose
   '8':    '2rem',       // 32px xloose
   '10':   '2.5rem',     // 40px xxloose
