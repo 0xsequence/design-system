68a91accc5fbdef50cf3e2a0afb0e49d161a19c0
Shawn Yeung <sy@horizon.io>
2022-11-17 15:38:46 -0500

    test docs

---
+++ docs/assets/Avatar.stories.b5b1798e.js
@@ -0,0 +1,26 @@
+var e=Object.defineProperty;var a=(o,t)=>e(o,"name",{value:t,configurable:!0});import{B as s}from"./Box.b0cabf01.js";import{A as r}from"./Avatar.d06ee0d3.js";import{j as n}from"./jsx-runtime.02798405.js";import"./atoms.css.9060149a.js";/* empty css                              */import"./index.6661eb55.js";import"./iframe.a4d5217f.js";const v={parameters:{storySource:{source:`import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Box } from '../Box'
+
+import { Avatar } from './Avatar'
+
+export default {
+  title: 'Components/Avatar',
+  component: Avatar,
+} as ComponentMeta<typeof Avatar>
+
+const Template: ComponentStory<typeof Avatar> = args => {
+  return (
+    <Box padding="3" background="backgroundSecondary" borderRadius="md">
+      <Avatar {...args} />
+    </Box>
+  )
+}
+
+export const A = Template.bind({})
+A.args = {
+  address: 'casdasfasfkasgfkasgf',
+  size: 'md',
+}
+`,locationsMap:{a:{startLoc:{col:48,line:12},endLoc:{col:1,line:18},startBody:{col:48,line:12},endBody:{col:1,line:18}}}}},title:"Components/Avatar",component:r},d=a(o=>n(s,{padding:"3",background:"backgroundSecondary",borderRadius:"md",children:n(r,{...o})}),"Template"),m=d.bind({});m.args={address:"casdasfasfkasgfkasgf",size:"md"};const x=["A"];export{m as A,x as __namedExportsOrder,v as default};
+//# sourceMappingURL=Avatar.stories.b5b1798e.js.map
