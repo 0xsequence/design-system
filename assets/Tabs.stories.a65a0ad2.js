cffa4309ab624d80872d3c23020c48753ca99bb8
corbanbrook <corbanbrook@gmail.com>
2022-11-21 18:50:51 +0000

    Deploying to gh-pages from  @ b105241a769c5ce46ccdde1636fef64675da0849 🚀

---
+++ assets/Tabs.stories.a65a0ad2.js
@@ -0,0 +1,31 @@
+var s=Object.defineProperty;var o=(t,e)=>s(t,"name",{value:e,configurable:!0});import{T as n}from"./Tabs.70555195.js";import{j as l}from"./jsx-runtime.d8a79987.js";import"./index.module.227b8335.js";import"./index.67548f43.js";import"./iframe.a146f067.js";import"./index.module.a5bf5bb5.js";import"./Text.751e87e9.js";import"./Box.4ba0cc8b.js";/* empty css                             */import"./atoms.css.cff455f3.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm.e63f122f.js";const v={parameters:{storySource:{source:`import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Tabs } from './Tabs'
+
+export default {
+  title: 'Components/Tabs',
+  component: Tabs
+} as ComponentMeta<typeof Tabs>
+
+const Template: ComponentStory<typeof Tabs> = args => {
+  return <Tabs {...args} />
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  defaultValue: 'coins',
+  tabs: [
+    {
+      value: 'coins',
+      title: 'Coins',
+      content: 'Coins'
+    },
+    {
+      value: 'collectibles',
+      title: 'Collectibles',
+      content: 'Collectibles'
+    }
+  ]
+}
+`,locationsMap:{default:{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}}}}},title:"Components/Tabs",component:n},a=o(t=>l(n,{...t}),"Template"),r=a.bind({});r.args={defaultValue:"coins",tabs:[{value:"coins",title:"Coins",content:"Coins"},{value:"collectibles",title:"Collectibles",content:"Collectibles"}]};const D=["Default"];export{r as Default,D as __namedExportsOrder,v as default};
+//# sourceMappingURL=Tabs.stories.a65a0ad2.js.map
