da4c39a1d1476e3bffc24e3668b096236a294eea
Corban Riley <corbanbrook@gmail.com>
2022-11-22 16:45:51 -0500

    Adding more placeholder icons

---
+++ src/icons/WarningIcon.tsx
@@ -0,0 +1,17 @@
+// Auto-generated file created by svgr-cli source /src/icons/template.js
+// Run yarn build:icons to update
+import { SVGProps } from 'react'
+
+import { Box } from '~/components/Box'
+
+import { IconProps } from './types'
+
+const Svg = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="1 -1 50 50" {...props}>
+    <path d="M12.094 1C7.394 1.8 3.8 5.394 3 10.094l4 .594c.4-3 2.688-5.288 5.688-5.688l-.594-4zM18 1v4h6V1h-6zm11 0v4h6V1h-6zm10.906 0-.593 4c2.9.4 5.187 2.688 5.687 5.688l4-.594C48.2 5.394 44.606 1.7 39.906 1zM3 15v6h4v-6H3zm42 1v6h4v-6h-4zM3 26v6h4v-6H3zm42 1v6h4v-6h-4zM7 37.313l-4 .593c.8 4.7 4.394 8.394 9.094 9.094l.594-4C9.787 42.6 7.5 40.312 7 37.312zm38 0c-.4 2.9-2.688 5.187-5.688 5.687l.594 4c4.7-.8 8.294-4.394 9.094-9.094l-4-.593zM17 43v4h6v-4h-6zm11 0v4h6v-4h-6z" />
+  </svg>
+)
+
+const SvgWarningIcon = (props: IconProps) => <Box as={Svg} {...props} />
+
+export default SvgWarningIcon
