f2da8900f958b24f391657c8a6c02b915042344e
Corban Riley <corbanbrook@gmail.com>
2022-11-22 11:51:50 -0500

    Fixing Stack component

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -3,15 +3,10 @@ import { Box, BoxProps } from '~/components/Box'
 interface StackProps extends BoxProps {}
 
 export const Stack = (props: StackProps) => {
-  const { children, flexDirection, gap } = props
+  const { children, flexDirection = 'column', gap = '2', ...rest } = props
 
   return (
-    <Box
-      display="flex"
-      color="textBody"
-      flexDirection={flexDirection}
-      gap={gap}
-    >
+    <Box display="flex" flexDirection={flexDirection} gap={gap} {...rest}>
       {children}
     </Box>
   )
