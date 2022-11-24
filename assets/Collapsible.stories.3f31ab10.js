713025620cd71ef9361a68d622ced869a4303c2d
YeungPhenom <syeung31@gmail.com>
2022-11-24 20:14:49 +0000

    Deploying to gh-pages from  @ 27bf273c5d406fe8b3946b6cedf386cab4f4c4fe 🚀

---
+++ assets/Collapsible.stories.3f31ab10.js
@@ -0,0 +1,26 @@
+var l=Object.defineProperty;var t=(o,n)=>l(o,"name",{value:n,configurable:!0});import{T as a}from"./Text.80097401.js";import{C as e}from"./Collapsible.0e3f5b82.js";import{a as r,j as p}from"./jsx-runtime.0d09e0bf.js";import"./Box.86297f26.js";/* empty css                             */import"./atoms.css.4b799120.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm.e63f122f.js";import"./index.module.5fc530f0.js";import"./index.402fd342.js";import"./iframe.25ff4bfc.js";import"./index.module.c06cc200.js";import"./WarningIcon.eb902138.js";import"./index.28e50838.js";const j={parameters:{storySource:{source:`import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Text } from '../Text'
+
+import { Collapsible } from './Collapsible'
+
+export default {
+  title: 'Components/Collapsible',
+  component: Collapsible,
+} as ComponentMeta<typeof Collapsible>
+
+const Template: ComponentStory<typeof Collapsible> = args => (
+  <Collapsible {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  label: 'My Heading',
+  children: [1, 2, 3, 4, 5].map(x => (
+    <Text variant="normal" as="p" color="textBody" key={x}>
+      Item {x}
+    </Text>
+  )),
+}
+`,locationsMap:{default:{startLoc:{col:53,line:12},endLoc:{col:1,line:14},startBody:{col:53,line:12},endBody:{col:1,line:14}}}}},title:"Components/Collapsible",component:e},i=t(o=>p(e,{...o}),"Template"),s=i.bind({});s.args={label:"My Heading",children:[1,2,3,4,5].map(o=>r(a,{variant:"normal",as:"p",color:"textBody",children:["Item ",o]},o))};const S=["Default"];export{s as Default,S as __namedExportsOrder,j as default};
+//# sourceMappingURL=Collapsible.stories.3f31ab10.js.map
---
+++ assets/Switch.stories.f9ba93d3.js
@@ -0,0 +1,17 @@
+var r=Object.defineProperty;var o=(t,e)=>r(t,"name",{value:e,configurable:!0});import{S as n}from"./Switch.e26bf8b7.js";import{j as i}from"./jsx-runtime.0d09e0bf.js";import"./index.module.5fc530f0.js";import"./index.402fd342.js";import"./iframe.25ff4bfc.js";import"./index.module.4b3982ac.js";/* empty css                             */const h={parameters:{storySource:{source:`import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Switch } from './Switch'
+
+export default {
+  title: 'Components/Switch',
+  component: Switch,
+} as ComponentMeta<typeof Switch>
+
+const Template: ComponentStory<typeof Switch> = args => {
+  return <Switch {...args} />
+}
+
+export const Default = Template.bind({})
+Default.args = {}
+`,locationsMap:{default:{startLoc:{col:48,line:10},endLoc:{col:1,line:12},startBody:{col:48,line:10},endBody:{col:1,line:12}}}}},title:"Components/Switch",component:n},a=o(t=>i(n,{...t}),"Template"),c=a.bind({});c.args={};const w=["Default"];export{c as Default,w as __namedExportsOrder,h as default};
+//# sourceMappingURL=Switch.stories.f9ba93d3.js.map
