f47237fe36727be7d0f453175e4a3ab729e4080e
YeungPhenom <syeung31@gmail.com>
2022-11-25 21:17:05 +0000

    Deploying to gh-pages from  @ 589e526ed1c30c662e58feddfd935c841cd42e3e 🚀

---
+++ assets/Tabs.stories.28131f35.js
@@ -0,0 +1,31 @@
+var s=Object.defineProperty;var o=(t,e)=>s(t,"name",{value:e,configurable:!0});import{T as n}from"./Tabs.55046596.js";import{j as l}from"./jsx-runtime.e149d0c1.js";import"./index.module.18e8d439.js";import"./index.8cd287ee.js";import"./iframe.8beaf75c.js";import"./index.module.98bb05e4.js";import"./Text.f4cb402b.js";import"./Box.8e5be3a0.js";/* empty css                             */import"./atoms.css.bb66546d.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm.e63f122f.js";const v={parameters:{storySource:{source:`import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Tabs } from './Tabs'
+
+export default {
+  title: 'Components/Tabs',
+  component: Tabs
+} as ComponentMeta<typeof Tabs>
+
+const Template: ComponentStory<typeof Tabs> = args => {
+  return <Tabs {...args} />
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  defaultValue: 'coins',
+  tabs: [
+    {
+      value: 'coins',
+      title: 'Coins',
+      content: 'Coins'
+    },
+    {
+      value: 'collectibles',
+      title: 'Collectibles',
+      content: 'Collectibles'
+    }
+  ]
+}
+`,locationsMap:{default:{startLoc:{col:46,line:10},endLoc:{col:1,line:12},startBody:{col:46,line:10},endBody:{col:1,line:12}}}}},title:"Components/Tabs",component:n},a=o(t=>l(n,{...t}),"Template"),r=a.bind({});r.args={defaultValue:"coins",tabs:[{value:"coins",title:"Coins",content:"Coins"},{value:"collectibles",title:"Collectibles",content:"Collectibles"}]};const D=["Default"];export{r as Default,D as __namedExportsOrder,v as default};
+//# sourceMappingURL=Tabs.stories.28131f35.js.map
---
+++ assets/TokenImage.stories.f8e38797.js
@@ -0,0 +1,26 @@
+var m=Object.defineProperty;var e=(o,a)=>m(o,"name",{value:a,configurable:!0});import{T as n}from"./TokenImage.51b4d43e.js";import{j as s}from"./jsx-runtime.e149d0c1.js";import"./Box.8e5be3a0.js";/* empty css                             */import"./atoms.css.bb66546d.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm.e63f122f.js";import"./iframe.8beaf75c.js";const y={parameters:{storySource:{source:`import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { TokenImage } from './TokenImage'
+
+export default {
+  title: 'Components/TokenImage',
+  component: TokenImage,
+} as ComponentMeta<typeof TokenImage>
+
+const Template: ComponentStory<typeof TokenImage> = args => {
+  return <TokenImage {...args} />
+}
+
+export const WithImage = Template.bind({})
+WithImage.args = {
+  size: 'lg',
+  src: 'https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912',
+}
+
+export const WithoutImage = Template.bind({})
+WithoutImage.args = {
+  size: 'lg',
+  symbol: 'SCAM',
+}
+`,locationsMap:{"with-image":{startLoc:{col:52,line:10},endLoc:{col:1,line:12},startBody:{col:52,line:10},endBody:{col:1,line:12}},"without-image":{startLoc:{col:52,line:10},endLoc:{col:1,line:12},startBody:{col:52,line:10},endBody:{col:1,line:12}}}}},title:"Components/TokenImage",component:n},t=e(o=>s(n,{...o}),"Template"),i=t.bind({});i.args={size:"lg",src:"https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912"};const r=t.bind({});r.args={size:"lg",symbol:"SCAM"};const u=["WithImage","WithoutImage"];export{i as WithImage,r as WithoutImage,u as __namedExportsOrder,y as default};
+//# sourceMappingURL=TokenImage.stories.f8e38797.js.map
