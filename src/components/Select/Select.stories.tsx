0069c3bfb6762d61286ce8f46ddd2194993e6326
Shawn Yeung <sy@horizon.io>
2022-11-28 10:55:45 -0500

    initial commit

---
+++ src/components/Select/Select.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Box } from '~/components/Box'
+
+import { Select } from './Select'
+
+export default {
+  title: 'Components/Select',
+  component: Select,
+} as ComponentMeta<typeof Select>
+
+const Template: ComponentStory<typeof Select> = args => {
+  return (
+    <Box padding="3" background="backgroundSecondary" borderRadius="md">
+      <Select {...args} />
+    </Box>
+  )
+}
+
+export const Demo = Template.bind({})
