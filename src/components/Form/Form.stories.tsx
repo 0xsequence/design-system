eccbe56825017cd4134feae63cf943e7df32f0fc
Shawn Yeung <sy@horizon.io>
2022-11-29 15:23:27 -0500

    fix form story

--- src/components/Form/Form.stories.tsx
+++ src/components/Form/Form.stories.tsx
@@ -11,40 +11,39 @@ export default {
   component: Form,
 } as ComponentMeta<typeof Form>
 
-const Template: Story = ({ ...args }) => {
-  const onSubmit = (data: {}) => console.log(data)
+const Template: Story = ({ onSubmit, ...args }) => (
+  <Box background="backgroundSecondary" padding="4" borderRadius="md">
+    <Form
+      background="backgroundPrimary"
+      flexDirection="column"
+      gap="3"
+      padding="4"
+      borderRadius="xs"
+      onSubmit={onSubmit}
+      {...args}
+    >
+      <TextInput
+        label="First Name"
+        labelLocation="top"
+        name="firstName"
+        placeholder="Enter first name"
+        registerWith={{ required: 'required' }}
+      />
+      <TextInput
+        label="Last Name"
+        labelLocation="top"
+        name="lastName"
+        placeholder="Enter last name"
+      />
 
-  return (
-    <Box background="backgroundSecondary" padding="4" borderRadius="md">
-      <Form
-        background="backgroundPrimary"
-        flexDirection="column"
-        gap="3"
-        padding="4"
-        borderRadius="xs"
-        onSubmit={onSubmit}
-        {...args}
-      >
-        <TextInput
-          label="First Name"
-          labelLocation="top"
-          name="firstName"
-          placeholder="Enter first name"
-          registerWith={{ required: 'required' }}
-        />
-        <TextInput
-          label="Last Name"
-          labelLocation="top"
-          name="lastName"
-          placeholder="Enter last name"
-        />
-
-        <Box flexDirection="row-reverse" marginTop="2">
-          <Button type="submit" label="Submit" />
-        </Box>
-      </Form>
-    </Box>
-  )
-}
+      <Box flexDirection="row-reverse" marginTop="2">
+        <Button type="submit" label="Submit" />
+      </Box>
+    </Form>
+  </Box>
+)
 
 export const Demo = Template.bind({})
+Demo.args = {
+  onSubmit: (data: {}) => console.log(data),
+}
