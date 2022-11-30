61d399b14daa421ee8fc2e477e7902df9c44afff
Shawn Yeung <sy@horizon.io>
2022-11-29 21:21:09 -0500

    controlled inputs

--- src/components/Form/Form.stories.tsx
+++ src/components/Form/Form.stories.tsx
@@ -2,7 +2,8 @@ import { ComponentMeta, Story } from '@storybook/react'
 
 import { Box } from '~/components/Box'
 import { Button } from '~/components/Button'
-import { TextInput } from '~/components/TextInput'
+import { Text } from '~/components/Text'
+import { ControlledTextInput } from '~/components/TextInput'
 
 import { Form } from './Form'
 
@@ -13,32 +14,46 @@ export default {
 
 const Template: Story = ({ onSubmit, ...args }) => (
   <Box background="backgroundSecondary" padding="4" borderRadius="md">
-    <Form
-      background="backgroundPrimary"
-      flexDirection="column"
-      gap="3"
-      padding="4"
-      borderRadius="xs"
-      onSubmit={onSubmit}
-      {...args}
-    >
-      <TextInput
-        label="First Name"
-        labelLocation="top"
-        name="firstName"
-        placeholder="Enter first name"
-        rules={{ required: 'This field is required.' }}
-      />
-      <TextInput
-        label="Last Name"
-        labelLocation="top"
-        name="lastName"
-        placeholder="Enter last name"
-      />
+    <Form defaultValues={{ firstName: '' }} onSubmit={onSubmit} {...args}>
+      {({ control, reset, formState: { errors } }) => (
+        <Box
+          background="backgroundPrimary"
+          flexDirection="column"
+          gap="3"
+          padding="4"
+          borderRadius="xs"
+        >
+          <ControlledTextInput
+            control={control}
+            name="firstName"
+            label="First Name"
+            labelLocation="top"
+            placeholder="Enter first name"
+            rules={{ required: 'First name is required' }}
+          />
+          {errors.firstName?.message && (
+            <Text color="negative">{errors.firstName.message as string}</Text>
+          )}
 
-      <Box flexDirection="row-reverse" marginTop="2">
-        <Button type="submit" label="Submit" />
-      </Box>
+          <ControlledTextInput
+            control={control}
+            name="lastName"
+            label="Last Name"
+            labelLocation="top"
+            placeholder="Enter last name"
+            rules={{ required: 'Last name is required' }}
+            defaultValue=""
+          />
+          {errors.lastName?.message && (
+            <Text color="negative">{errors.lastName.message as string}</Text>
+          )}
+
+          <Box flexDirection="row-reverse" marginTop="2" gap="2">
+            <Button type="submit" label="Submit" variant="primary" />
+            <Button type="reset" label="Reset" onClick={() => reset()} />
+          </Box>
+        </Box>
+      )}
     </Form>
   </Box>
 )
