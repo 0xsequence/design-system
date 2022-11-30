2790d012da2c3300f9e5aa02b280bb0365cff95f
Shawn Yeung <sy@horizon.io>
2022-11-30 13:15:21 -0500

    add controlled switch

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,4 +1,5 @@
 import * as SwitchPrimitive from '@radix-ui/react-switch'
+import { Control, Controller } from 'react-hook-form'
 
 import * as styles from './styles.css'
 
@@ -9,3 +10,34 @@ export const Switch = (props: SwitchPrimitive.SwitchProps) => {
     </SwitchPrimitive.Root>
   )
 }
+
+type ControlledSwitchProps = SwitchPrimitive.SwitchProps & {
+  control: Control
+  name: string
+  onCheckedChange: (checked: boolean) => void
+  rules?: {}
+}
+
+export const ControlledSwitch = ({
+  control,
+  defaultChecked,
+  onCheckedChange,
+  name,
+  rules,
+  ...switchProps
+}: ControlledSwitchProps) => (
+  <Controller
+    defaultValue={defaultChecked}
+    name={name}
+    control={control}
+    rules={rules}
+    render={({ field: { ref: _, ...fieldProps } }) => (
+      <Switch
+        onCheckedChange={onCheckedChange}
+        defaultChecked={defaultChecked}
+        {...fieldProps}
+        {...switchProps}
+      />
+    )}
+  />
+)
--- src/components/Switch/index.ts
+++ src/components/Switch/index.ts
@@ -1,1 +1,1 @@
-export { Switch } from './Switch'
+export { ControlledSwitch, Switch } from './Switch'
