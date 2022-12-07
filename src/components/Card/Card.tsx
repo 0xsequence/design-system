c36a15fea316aee83757a08f56724d56484dcf6b
Corban Brook <corbanbrook@gmail.com>
2022-12-07 11:41:49 -0500

    Card component (#49)

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,47 @@
+import { ElementType, forwardRef } from 'react'
+
+import {
+  Box,
+  PolymorphicComponent,
+  PolymorphicProps,
+  PolymorphicRef,
+} from '../Box'
+
+import * as styles from './styles.css'
+
+interface CardProps {
+  clickable?: boolean
+  outlined?: boolean
+  disabled?: boolean
+  blur?: boolean
+}
+
+export const Card: PolymorphicComponent<CardProps, 'div'> = forwardRef(
+  <T extends ElementType>(
+    props: PolymorphicProps<CardProps, T>,
+    ref: PolymorphicRef<T>
+  ) => {
+    const {
+      children,
+      clickable,
+      outlined,
+      disabled,
+      blur,
+      width = 'full',
+      ...rest
+    } = props
+    return (
+      <Box
+        className={styles.cardVariants({ clickable, outlined, disabled, blur })}
+        background={outlined ? 'transparent' : 'backgroundSecondary'}
+        borderRadius="md"
+        padding="4"
+        width={width}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </Box>
+    )
+  }
+)
---
+++ src/components/Card/styles.css.ts
@@ -0,0 +1,36 @@
+import { recipe, RecipeVariants } from '@vanilla-extract/recipes'
+
+import { atoms } from '~/css'
+
+export const cardVariants = recipe({
+  variants: {
+    clickable: {
+      true: atoms({
+        opacity: {
+          hover: '80',
+        },
+        cursor: 'pointer',
+      }),
+    },
+
+    disabled: {
+      true: { opacity: 0.5, cursor: 'not-allowed', pointerEvents: 'none' },
+    },
+
+    outlined: {
+      true: atoms({
+        borderStyle: 'solid',
+        borderWidth: 'thin',
+        borderColor: 'borderNormal',
+      }),
+    },
+
+    blur: {
+      true: atoms({
+        backdropFilter: 'blur',
+      }),
+    },
+  },
+})
+
+export type CardVariants = RecipeVariants<typeof cardVariants>
