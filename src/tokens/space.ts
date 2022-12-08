d5722533cbc81dda68d9a13be670f0010fad6cc9
Corban Riley <corbanbrook@gmail.com>
2022-12-08 14:17:50 -0500

    width and height atoms now can use space values

--- src/tokens/space.ts
+++ src/tokens/space.ts
@@ -1,16 +1,18 @@
 // prettier-ignore
 export const space = {
-  '0':    '0',          // 0px  none
+  '0':    '0',          // 0px
   '0.25': '0.0625rem',  // 1px
   '0.5':  '0.125rem',   // 2px
-  '1':    '0.25rem',    // 4px  xxtight
+  '1':    '0.25rem',    // 4px
   '1.5':  '0.375rem',   // 6px
-  '2':    '0.5rem',     // 8px  xtight
-  '3':    '0.75rem',    // 12px tight
-  '4':    '1rem',       // 16px normal
+  '2':    '0.5rem',     // 8px
+  '3':    '0.75rem',    // 12px
+  '4':    '1rem',       // 16px
   '5':    '1.25rem',    // 20px
-  '6':    '1.5rem',     // 24px loose
-  '8':    '2rem',       // 32px xloose
-  '10':   '2.5rem',     // 40px xxloose
-  '12':   '3rem',       // 48px huge
+  '6':    '1.5rem',     // 24px
+  '8':    '2rem',       // 32px
+  '10':   '2.5rem',     // 40px
+  '12':   '3rem',       // 48px
+  '14':   '3.5rem',     // 56px
+  '16':   '4rem',       // 64px
 }
