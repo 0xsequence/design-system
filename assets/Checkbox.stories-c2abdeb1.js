247d66dd9151454cbb5163bbb8b91ce10f8160ef
corbanbrook <corbanbrook@gmail.com>
2022-12-17 16:33:22 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@ef7714b55cc4784439b6619e99f27ef4e706da64 🚀

---
+++ assets/Checkbox.stories-c2abdeb1.js
@@ -0,0 +1,22 @@
+var r=Object.defineProperty;var e=(o,n)=>r(o,"name",{value:n,configurable:!0});import{C as t}from"./Checkbox-c3af7b07.js";import{j as i}from"./jsx-runtime-6057fa59.js";import"./index.module-ab67e808.js";import"./index-9b47cd5d.js";import"./es.object.get-own-property-descriptor-a95ea412.js";import"./index-7bfbb1c9.js";import"./index.module-4b1ee413.js";import"./index.module-adb0a49a.js";import"./index.module-79bb90fa.js";import"./Field-05b9f6d4.js";import"./Box-0ba2098e.js";/* empty css                             */import"./atoms.css-36c6db2d.js";import"./Text-dbab31a0.js";import"./typography-f274f330.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm-1de2ed83.js";import"./WarningIcon-58080c2d.js";const _={parameters:{storySource:{source:`import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Checkbox } from './Checkbox'
+
+export default {
+  title: 'Forms/Checkbox',
+  component: Checkbox,
+} as ComponentMeta<typeof Checkbox>
+
+const Template: ComponentStory<typeof Checkbox> = args => {
+  return <Checkbox {...args} />
+}
+
+export const Demo = Template.bind({})
+Demo.args = {
+  label: 'This is the label (CheckmarkIcon is missing)',
+  labelLocation: 'right',
+  size: 'sm',
+  disabled: false,
+}
+`,locationsMap:{demo:{startLoc:{col:50,line:10},endLoc:{col:1,line:12},startBody:{col:50,line:10},endBody:{col:1,line:12}}}}},title:"Forms/Checkbox",component:t},m=e(o=>i(t,{...o}),"Template"),s=m.bind({});s.args={label:"This is the label (CheckmarkIcon is missing)",labelLocation:"right",size:"sm",disabled:!1};const j=["Demo"];export{s as Demo,j as __namedExportsOrder,_ as default};
+//# sourceMappingURL=Checkbox.stories-c2abdeb1.js.map
