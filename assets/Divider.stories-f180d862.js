e99335a1a9b17259f727dc76d8778482c38d4697
YeungPhenom <syeung31@gmail.com>
2022-12-19 22:47:05 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@b899cf477d8cfe14bf3586c0f953f373eeb84871 🚀

---
+++ assets/Divider.stories-f180d862.js
@@ -0,0 +1,18 @@
+var n=Object.defineProperty;var o=(t,e)=>n(t,"name",{value:e,configurable:!0});import{B as i}from"./Box-0ba2098e.js";import{D as m}from"./Divider-09b9bfd2.js";import{j as r}from"./jsx-runtime-6057fa59.js";import"./index-9b47cd5d.js";import"./es.object.get-own-property-descriptor-a95ea412.js";/* empty css                             */import"./atoms.css-36c6db2d.js";const u={parameters:{storySource:{source:`import { Box } from '../Box'
+
+import { Divider } from './Divider'
+
+export default {
+  title: 'Components/Divider',
+  component: 'Divider',
+}
+
+export const Default = () => {
+  return (
+    <Box>
+      <Divider />
+    </Box>
+  )
+}
+`,locationsMap:{default:{startLoc:{col:23,line:10},endLoc:{col:1,line:16},startBody:{col:23,line:10},endBody:{col:1,line:16}}}}},title:"Components/Divider",component:"Divider"},x=o(()=>r(i,{children:r(m,{})}),"Default"),v=["Default"];export{x as Default,v as __namedExportsOrder,u as default};
+//# sourceMappingURL=Divider.stories-f180d862.js.map
