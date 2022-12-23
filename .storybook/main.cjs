3ee4f611da64da4132d528e3aabbeb8b79523ce4
Corban Riley <corbanbrook@gmail.com>
2022-12-23 16:38:41 -0500

    Cleanup storybook main - no longer need vite config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,8 +1,3 @@
-const path = require('path')
-const { mergeConfig } = require('vite')
-const { vanillaExtractPlugin } = require('@vanilla-extract/vite-plugin')
-const { default: tsconfigPaths } = require('vite-tsconfig-paths')
-
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
@@ -11,25 +6,6 @@ module.exports = {
     options: {},
   },
   staticDirs: ['../public'],
-  // async viteFinal(config, { configType }) {
-  //   return mergeConfig(config, {
-  //     base: configType === 'PRODUCTION' ? './' : config.base,
-  //     plugins: [
-  //       tsconfigPaths(),
-  //       vanillaExtractPlugin({
-  //         identifiers: 'short',
-  //       }),
-  //     ],
-  //     resolve: {
-  //       alias: [
-  //         {
-  //           find: '~',
-  //           replacement: path.resolve(__dirname, '../src'),
-  //         },
-  //       ],
-  //     },
-  //   })
-  // },
   // docs: {
   //  docsPage: 'automatic'
   // }
