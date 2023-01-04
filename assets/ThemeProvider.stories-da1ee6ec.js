e226e766c9ee3860ccf9b122bffa645d022a545a
corbanbrook <corbanbrook@gmail.com>
2023-01-04 15:58:58 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@273872ccfa42586af6735f8e04e1b6b2a057508e 🚀

---
+++ assets/ThemeProvider.stories-da1ee6ec.js
@@ -0,0 +1,11 @@
+import{j as m}from"./jsx-runtime-a9c13c85.js";import"./Box-445c644d.js";import"./Avatar-4378ff32.js";import"./Badge-2a988710.js";import"./Breadcrumbs-b30d23f9.js";import{B as i}from"./Button-14f1eb54.js";/* empty css                             *//* empty css                              */import"./Card-e0fbfa6a.js";import"./Checkbox-1bccce1a.js";import"./ControlledTextInput-9c811d59.js";import{T as p,u as s}from"./Tooltip-52d1e3cf.js";import"./FileInput-87db9a96.js";import"./IconButton-2915d04b.js";import"./Field-a1754365.js";import"./Modal-93f1c915.js";import"./RadioGroup-d81c6bf5.js";import"./Scroll-4399c8a2.js";import"./Select-a2b85940.js";import"./Spinner-bdc1efca.js";import"./Switch-76ad5feb.js";import"./TabbedNav-7f160911.js";import"./Tag-cf82bcac.js";import"./Text-61f81ce4.js";import"./TokenImage-98182aeb.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./atoms.css-4912cd8f.js";import"./index-4d501b15.js";import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./Divider-978ad61e.js";import"./extends-98964cd2.js";import"./index.module-ad4057e5.js";import"./index-ffd4619b.js";import"./index.module-e6a6661a.js";import"./index.module-19695c6a.js";import"./index.module-f4493aef.js";import"./WarningIcon-6812aaa3.js";import"./index.module-c986f64a.js";import"./index.module-bab4cba0.js";import"./Combination-adb67df6.js";import"./typography-f274f330.js";const Y={title:"Components/ThemeProvider",component:p},t=()=>{const{theme:r,setTheme:e}=s();return m(i,{label:"Toggle theme",onClick:()=>{e(r==="light"?"dark":"light")}})};var o;t.parameters={...t.parameters,storySource:{source:`() => {
+  const {
+    theme,
+    setTheme
+  } = useTheme();
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+  return <Button label="Toggle theme" onClick={toggleTheme} />;
+}`,...(o=t.parameters)==null?void 0:o.storySource}};const Z=["Default"];export{t as Default,Z as __namedExportsOrder,Y as default};
+//# sourceMappingURL=ThemeProvider.stories-da1ee6ec.js.map
