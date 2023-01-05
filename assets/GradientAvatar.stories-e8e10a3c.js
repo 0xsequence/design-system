bb83108a3ad711e3b17f3b18f20d7d30ff44725b
corbanbrook <corbanbrook@gmail.com>
2023-01-05 03:59:53 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@e08f1bc43dd764051ef576084fd7828932a04cd6 🚀

---
+++ assets/GradientAvatar.stories-e8e10a3c.js
@@ -0,0 +1,28 @@
+import{a,F as o,j as r}from"./jsx-runtime-a9c13c85.js";import{B as t}from"./Box-f4432bb8.js";import"./Avatar-5a2bae22.js";import"./Badge-9ae21610.js";import"./Breadcrumbs-d147e47e.js";import"./Button-c4c91c81.js";import"./atoms.css-bb4ad6bf.js";/* empty css                              */import"./Card-764433ae.js";import"./Checkbox-229d09e1.js";import"./ControlledSwitch-65cd1fb5.js";import{G as e}from"./Tooltip-99a2f0ff.js";import"./FileInput-a7fdbaeb.js";import"./IconButton-99437871.js";import"./Field-a22ff8d4.js";import"./Modal-00e0f10c.js";import"./NumericInput-58cc26f7.js";import"./RadioGroup-5b163566.js";import"./Scroll-e51eaffc.js";import"./Select-04e13ef2.js";import"./Spinner-c1ee5488.js";import"./Switch-0a9f403d.js";import"./TabbedNav-c478a286.js";import"./Tag-57f86961.js";import"./Text-bae74cde.js";import"./ControlledTextInput-3a3b904e.js";import"./TokenImage-23a6913d.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-4d501b15.js";import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./Divider-97b2a29f.js";import"./extends-98964cd2.js";import"./index.module-ad4057e5.js";import"./index-ffd4619b.js";import"./index.module-e6a6661a.js";import"./index.module-19695c6a.js";import"./index.module-f4493aef.js";import"./WarningIcon-d09bba5b.js";import"./index.module-c986f64a.js";import"./index.module-bab4cba0.js";import"./Combination-adb67df6.js";import"./typography-f274f330.js";const Z={title:"Components/GradientAvatar",component:e},d={args:{address:"0x1234567890123456789012345678901234567890"}},s=()=>a(o,{children:[a(t,{gap:"4",children:[r(e,{address:"0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8B9",size:"xl"}),r(e,{address:"0x73F8E47dEe63002378E047fA74e6DFb24F88f496",size:"xl"}),r(e,{address:"0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECD0",size:"xl"}),r(e,{address:"0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1e",size:"xl"}),r(e,{address:"0x9aB58Bda3eeCbF391a17592ed7626727b922a18f",size:"xl"}),r(e,{address:"0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c04",size:"xl"}),r(e,{address:"0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4C",size:"xl"}),r(e,{address:"0x88cc3E896C4C178AB98333dCD29C33d2cc2102A8",size:"xl"})]}),a(t,{gap:"4",marginTop:"4",children:[r(e,{address:"0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8Bx",size:"xl"}),r(e,{address:"0x73F8E47dEe63002378E047fA74e6DFb24F88f49x",size:"xl"}),r(e,{address:"0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECDx",size:"xl"}),r(e,{address:"0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1x",size:"xl"}),r(e,{address:"0x9aB58Bda3eeCbF391a17592ed7626727b922a18x",size:"xl"}),r(e,{address:"0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c0x",size:"xl"}),r(e,{address:"0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4x",size:"xl"}),r(e,{address:"0x88cc3E896C4C178AB98333dCD29C33d2cc2102Ax",size:"xl"})]})]});var i;d.parameters={...d.parameters,storySource:{source:`{
+  args: {
+    address: '0x1234567890123456789012345678901234567890'
+  }
+}`,...(i=d.parameters)==null?void 0:i.storySource}};var x;s.parameters={...s.parameters,storySource:{source:`() => <>
+    <Box gap="4">
+      <GradientAvatar address="0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8B9" size="xl" />
+      <GradientAvatar address="0x73F8E47dEe63002378E047fA74e6DFb24F88f496" size="xl" />
+      <GradientAvatar address="0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECD0" size="xl" />
+      <GradientAvatar address="0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1e" size="xl" />
+      <GradientAvatar address="0x9aB58Bda3eeCbF391a17592ed7626727b922a18f" size="xl" />
+      <GradientAvatar address="0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c04" size="xl" />
+      <GradientAvatar address="0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4C" size="xl" />
+      <GradientAvatar address="0x88cc3E896C4C178AB98333dCD29C33d2cc2102A8" size="xl" />
+    </Box>
+
+    <Box gap="4" marginTop="4">
+      <GradientAvatar address="0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8Bx" size="xl" />
+      <GradientAvatar address="0x73F8E47dEe63002378E047fA74e6DFb24F88f49x" size="xl" />
+      <GradientAvatar address="0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECDx" size="xl" />
+      <GradientAvatar address="0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1x" size="xl" />
+      <GradientAvatar address="0x9aB58Bda3eeCbF391a17592ed7626727b922a18x" size="xl" />
+      <GradientAvatar address="0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c0x" size="xl" />
+      <GradientAvatar address="0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4x" size="xl" />
+      <GradientAvatar address="0x88cc3E896C4C178AB98333dCD29C33d2cc2102Ax" size="xl" />
+    </Box>
+  </>`,...(x=s.parameters)==null?void 0:x.storySource}};const $=["Default","Multiple"];export{d as Default,s as Multiple,$ as __namedExportsOrder,Z as default};
+//# sourceMappingURL=GradientAvatar.stories-e8e10a3c.js.map
---
+++ assets/NumericInput.stories-6a3c01d9.js
@@ -0,0 +1,9 @@
+import{j as t,F as p}from"./jsx-runtime-a9c13c85.js";import{r as n}from"./index-d8ade8ea.js";import{N as o}from"./NumericInput-58cc26f7.js";import"./_commonjsHelpers-042e6b4d.js";import"./ControlledTextInput-3a3b904e.js";import"./Box-f4432bb8.js";import"./atoms.css-bb4ad6bf.js";import"./Field-a22ff8d4.js";import"./Text-bae74cde.js";import"./typography-f274f330.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";const T={title:"Forms/NumericInput",component:o},m=s=>{const[i,a]=n.useState("");return t(p,{children:t(o,{...s,value:i,onChange:a})})},r={render:m,args:{label:"This the label",description:"This is the description",labelLocation:"top"}};var e;r.parameters={...r.parameters,storySource:{source:`{
+  render: StoryWrapper,
+  args: {
+    label: 'This the label',
+    description: 'This is the description',
+    labelLocation: 'top'
+  }
+}`,...(e=r.parameters)==null?void 0:e.storySource}};const F=["Default"];export{r as Default,F as __namedExportsOrder,T as default};
+//# sourceMappingURL=NumericInput.stories-6a3c01d9.js.map
