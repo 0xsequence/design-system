5167e43f843f5e929838b1a19524458daa7eebb2
corbanbrook <corbanbrook@gmail.com>
2023-01-11 16:24:11 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@a0926c08fa2e5f986700de17d39dc312af4586c9 🚀

---
+++ assets/TextInput.stories-119a2905.js
@@ -0,0 +1,43 @@
+import{j as i,a as b}from"./jsx-runtime-a9c13c85.js";import{B as g}from"./Box-6ce8bd92.js";import"./Avatar-44c525cf.js";import"./Badge-4d843247.js";import"./Breadcrumbs-a38e0f4a.js";import{B as l}from"./Button-5e68502d.js";import"./atoms.css-3866390b.js";/* empty css                              */import"./Card-55c8c6d5.js";import"./Checkbox-072213fe.js";import"./ControlledSwitch-f93c0c26.js";import"./Tooltip-fcc171ab.js";import"./index-1bf42c54.js";import"./DropdownMenu-b8c1adfe.js";import"./FileInput-0f1af378.js";import{I as T}from"./IconButton-bdb5d79a.js";import"./Field-d7745a6b.js";import"./Modal-f6de8eda.js";import"./RadioGroup-bc412375.js";import"./Scroll-b8e741f2.js";import"./Select-7b835365.js";import"./Spinner-0060a0dd.js";import"./StopClickPropagation-4dbdfe56.js";import"./Switch-55f30ced.js";import"./TabbedNav-a0b4080d.js";import"./Tag-fd580799.js";import"./Text-f680f5c3.js";import{T as S}from"./ControlledTextInput-4f06c38e.js";import"./TokenImage-c237468e.js";import{I,g as d,b as x,c as f}from"./WarningIcon-d2e01a38.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-4d501b15.js";import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./Divider-6c536a55.js";import"./extends-98964cd2.js";import"./index.module-d15610cf.js";import"./index-ffd4619b.js";import"./index.module-e6a6661a.js";import"./index.module-45828978.js";import"./index.module-732cca0e.js";import"./index.module-a9b21657.js";import"./NumericInput-f5d6548d.js";import"./index.module-fa6a82d0.js";import"./index.module-8c7796ea.js";import"./Combination-ae2baabd.js";import"./index.module-f17d4a85.js";import"./typography-f274f330.js";const be={title:"Forms/TextInput",component:S},e={args:{label:"This the label",description:"This is the description",placeholder:"This is the placeholder",labelLocation:"top"}},o={args:{label:"This the label",placeholder:"This is the placeholder",leftIcon:I}},r={args:{label:"This the label",placeholder:"This is the placeholder",rightIcon:d}},t={args:{label:"This the label",placeholder:"This is the placeholder",leftIcon:I,rightIcon:d}},s={args:{placeholder:"Search for coins or collectibles",leftIcon:x,controls:i(T,{icon:f,size:"xs"})}},a={args:{label:"This the label",placeholder:"This is the placeholder",controls:b(g,{gap:"2",children:[i(l,{label:"Max",size:"xs",shape:"square"}),i(l,{label:"Paste",size:"xs",shape:"square"})]})}};var p;e.parameters={...e.parameters,storySource:{source:`{
+  args: {
+    label: 'This the label',
+    description: 'This is the description',
+    placeholder: 'This is the placeholder',
+    labelLocation: 'top'
+  }
+}`,...(p=e.parameters)==null?void 0:p.storySource}};var c;o.parameters={...o.parameters,storySource:{source:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    leftIcon: ScanIcon
+  }
+}`,...(c=o.parameters)==null?void 0:c.storySource}};var h;r.parameters={...r.parameters,storySource:{source:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    rightIcon: ArrowRightIcon
+  }
+}`,...(h=r.parameters)==null?void 0:h.storySource}};var n;t.parameters={...t.parameters,storySource:{source:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    leftIcon: ScanIcon,
+    rightIcon: ArrowRightIcon
+  }
+}`,...(n=t.parameters)==null?void 0:n.storySource}};var m;s.parameters={...s.parameters,storySource:{source:`{
+  args: {
+    placeholder: 'Search for coins or collectibles',
+    leftIcon: SearchIcon,
+    controls: <IconButton icon={CloseIcon} size="xs" />
+  }
+}`,...(m=s.parameters)==null?void 0:m.storySource}};var u;a.parameters={...a.parameters,storySource:{source:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    controls: <Box gap="2">
+        <Button label="Max" size="xs" shape="square" />
+        <Button label="Paste" size="xs" shape="square" />
+      </Box>
+  }
+}`,...(u=a.parameters)==null?void 0:u.storySource}};const ge=["Default","WithLeftIcon","WithRightIcon","WithBothIcons","SearchInput","WithComplexControls"];export{e as Default,s as SearchInput,t as WithBothIcons,a as WithComplexControls,o as WithLeftIcon,r as WithRightIcon,ge as __namedExportsOrder,be as default};
+//# sourceMappingURL=TextInput.stories-119a2905.js.map
