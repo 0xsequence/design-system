7d2c17473aefa982bfef6d059efd9a1bc420c23b
corbanbrook <corbanbrook@gmail.com>
2023-01-11 21:15:18 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@672e47d5b964f0661b6a3b07f6a8f95ca82f67b4 🚀

---
+++ assets/Typography.stories-9f3eec6a.js
@@ -0,0 +1,24 @@
+import{M as c}from"./chunk-MA2MUXQN-4eca42bd.js";import"./chunk-R4NKYYJA-96bb58e6.js";import"./Box-5a71b8bf.js";import"./Avatar-4b701796.js";import"./Badge-b3e5081c.js";import"./Breadcrumbs-fa2784e0.js";import"./Button-2c965b19.js";import"./atoms.css-3866390b.js";/* empty css                              */import"./Card-6c51aae3.js";import"./Checkbox-50bcb0e2.js";import"./ControlledSwitch-4586c9af.js";import"./Tooltip-9dfbb9f3.js";import"./index-e096c804.js";import"./DropdownMenu-1ce8a621.js";import"./FileInput-1a1dae8c.js";import"./Field-1cca3cf5.js";import"./IconButton-dd6840d6.js";import"./TokenImage-94a7234a.js";import"./Modal-b211fa45.js";import"./RadioGroup-0aef3f43.js";import"./Scroll-33d154f8.js";import"./Select-cb68bc62.js";import"./Spinner-2f278bb9.js";import"./StopClickPropagation-09e2ae33.js";import"./Switch-7a729bf9.js";import"./TabbedNav-431dbf08.js";import"./Tag-a86c9ef0.js";import{T as i}from"./Text-2c61dd5f.js";import"./ControlledTextInput-65bd54a2.js";import{j as t,a as o,F as h}from"./jsx-runtime-a9c13c85.js";import{u as a}from"./index-f3daabb9.js";import"./preload-helper-41c905a7.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-ffd4619b.js";import"./chunk-GWAJ4KRU-d69fdc61.js";import"./index-5fbe3bd0.js";import"./index-d475d2ea.js";import"./memoizerific-a8c4a000.js";import"./index-e1ce60ba.js";import"./chunk-XHUUYXNA-e2d09062.js";import"./chunk-FD4M6EBV-8d27da22.js";import"./chunk-NNAAFZ4U-67fa674f.js";import"./chunk-BVZGY62N-610dc239.js";import"./_getTag-6acf5a83.js";import"./_baseIsEqual-0821e666.js";import"./extends-98964cd2.js";import"./index-639f2073.js";import"./index-4d501b15.js";import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./Divider-b5990c63.js";import"./index.module-d15610cf.js";import"./index.module-e6a6661a.js";import"./index.module-45828978.js";import"./index.module-732cca0e.js";import"./WarningIcon-f712c85e.js";import"./index.module-a9b21657.js";import"./motion-7df5a7b3.js";import"./NumericInput-0ba5082c.js";import"./index.module-fa6a82d0.js";import"./index.module-8c7796ea.js";import"./Combination-ae2baabd.js";import"./index.module-f17d4a85.js";import"./Image-a11094bb.js";import"./typography-f274f330.js";const e="The quick brown fox jumps over the lazy dog";function l(p={}){const{wrapper:m}=Object.assign({},a(),p.components);return m?t(m,{...p,children:t(s,{})}):s();function s(){const r=Object.assign({h1:"h1",p:"p",strong:"strong",code:"code"},a(),p.components);return o(h,{children:[t(c,{title:"Design System/Typography"}),`
+`,t(r.h1,{children:"Typography"}),`
+`,t(r.p,{children:"The defined set of typefaces and used in the design system."}),`
+`,o(r.p,{children:[t(r.strong,{children:"xlarge"}),`
+`,t(r.code,{children:"font-size: 30px, line-height: 36px, font-weight: 700"})]}),`
+`,t(i,{variant:"xlarge",children:e}),`
+`,o(r.p,{children:[t(r.strong,{children:"large"}),`
+`,t(r.code,{children:"font-size: 20px, line-height: 28px, font-weight: 600"})]}),`
+`,t(i,{variant:"large",children:e}),`
+`,o(r.p,{children:[t(r.strong,{children:"medium"}),`
+`,t(r.code,{children:"font-size: 16px, line-height: 24px, font-weight: 700"})]}),`
+`,t(i,{variant:"medium",children:e}),`
+`,o(r.p,{children:[t(r.strong,{children:"normal"}),`
+`,t(r.code,{children:"font-size: 14px, line-height: 20px, font-weight: 400"})]}),`
+`,t(i,{variant:"normal",children:e}),`
+`,o(r.p,{children:[t(r.strong,{children:"small"}),`
+`,t(r.code,{children:"font-size: 12px, line-height: 16px, font-weight: 500"})]}),`
+`,t(i,{variant:"small",children:e}),`
+`,o(r.p,{children:[t(r.strong,{children:"xsmall"}),`
+`,t(r.code,{children:"font-size: 10px, line-height: 16px, font-weight: 700"})]}),`
+`,t(i,{variant:"xsmall",children:e}),`
+`,t(r.p,{children:t(r.strong,{children:"code"})}),`
+`,t(i,{variant:"code",children:e})]})}}const d=()=>{throw new Error("Docs-only story")};d.parameters={docsOnly:!0};const n={title:"Design System/Typography",tags:["stories-mdx"],includeStories:["__page"]};n.parameters=n.parameters||{};n.parameters.docs={...n.parameters.docs||{},page:l};const Dt=["sampleText","__page"];export{Dt as __namedExportsOrder,d as __page,n as default,e as sampleText};
+//# sourceMappingURL=Typography.stories-9f3eec6a.js.map
