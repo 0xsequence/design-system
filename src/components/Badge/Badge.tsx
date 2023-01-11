a0926c08fa2e5f986700de17d39dc312af4586c9
Corban Riley <corbanbrook@gmail.com>
2023-01-11 11:21:56 -0500

    Adding BoxProps to Badge

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,24 +1,27 @@
+import { clsx } from 'clsx'
 import { ReactElement } from 'react'
 
-import { Box } from '~/components/Box'
+import { Box, BoxProps } from '~/components/Box'
 
 import { badgeVariants, BadgeVariants } from './styles.css'
 
-type BadgeProps = BadgeVariants & {
-  value: ReactElement | string | number
-}
+type BadgeProps = BoxProps &
+  BadgeVariants & {
+    value: ReactElement | string | number
+  }
 
 export const Badge = (props: BadgeProps) => {
-  const { value, variant = 'info', size = 'md' } = props
+  const { className, value, variant = 'info', size = 'md', ...rest } = props
 
   return (
     <Box
-      className={badgeVariants({ variant, size })}
+      className={clsx(className, badgeVariants({ variant, size }))}
       display="inline-flex"
       placeItems="center"
       borderRadius="circle"
       color="white"
       paddingX="1"
+      {...rest}
     >
       {value}
     </Box>
