6ce0a2a1d15afa091d7b8302312aad5ce67bd114
corbanbrook <corbanbrook@gmail.com>
2023-01-13 00:33:24 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@df9161f2aa48ee25112e8ec7acba13898bdd65df 🚀

---
+++ assets/Switch.stories-0116c739.js
@@ -0,0 +1,8 @@
+import{S as i}from"./Switch-31e896cc.js";import"./jsx-runtime-a9c13c85.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./extends-98964cd2.js";import"./index.module-d15610cf.js";import"./index-ffd4619b.js";import"./index.module-e6a6661a.js";import"./index.module-45828978.js";import"./Box-0be2affa.js";import"./atoms.css-8015a288.js";import"./Field-d47e485b.js";import"./Text-de632491.js";import"./typography-f274f330.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";const D={title:"Forms/Switch",component:i},t={args:{disabled:!1,label:"Switch Title",description:"Switch Description"}};var r;t.parameters={...t.parameters,storySource:{source:`{
+  args: {
+    disabled: false,
+    label: 'Switch Title',
+    description: 'Switch Description'
+  }
+}`,...(r=t.parameters)==null?void 0:r.storySource}};const _=["Default"];export{t as Default,_ as __namedExportsOrder,D as default};
+//# sourceMappingURL=Switch.stories-0116c739.js.map
