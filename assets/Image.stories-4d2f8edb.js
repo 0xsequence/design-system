879b3b88589e2ec5f27fd35a422761609f5e6bc1
YeungPhenom <syeung31@gmail.com>
2023-01-17 22:19:56 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@7ac3e33129e07d99dcf7d90505b69546861100b6 🚀

---
+++ assets/Image.stories-4d2f8edb.js
@@ -0,0 +1,6 @@
+import{I as r}from"./Image-d41e85a0.js";import"./jsx-runtime-a9c13c85.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./Box-71460fdd.js";import"./atoms.css-afddc020.js";import"./motion-7df5a7b3.js";const g={title:"Components/Image",component:r},e="https://skyweaver.ghost.io/content/images/size/w600/2023/01/hexbound_promo.png",t={args:{src:e}};var o;t.parameters={...t.parameters,storySource:{source:`{
+  args: {
+    src
+  }
+}`,...(o=t.parameters)==null?void 0:o.storySource}};const u=["Default"];export{t as Default,u as __namedExportsOrder,g as default};
+//# sourceMappingURL=Image.stories-4d2f8edb.js.map
---
+++ assets/Tabs.stories-4657dc17.js
@@ -0,0 +1,19 @@
+import{j as t}from"./jsx-runtime-a9c13c85.js";import"./Box-71460fdd.js";import"./Avatar-d122d221.js";import"./Badge-92d2755f.js";import"./Breadcrumbs-fb085f15.js";import"./Button-8adb6abb.js";import"./atoms.css-afddc020.js";/* empty css                              */import{C as n}from"./Card-ef4acde8.js";import"./Checkbox-5a69758f.js";import"./ControlledSwitch-34b24cf7.js";import{a as m}from"./Tooltip-f96aecf7.js";import"./index-aacf97be.js";import"./DropdownMenu-c4cd8d37.js";import"./FileInput-e1c1132d.js";import"./Field-2edc6e2f.js";import"./IconButton-2fef59d9.js";import"./TokenImage-e85e0118.js";import"./Modal-36480934.js";import"./RadioGroup-249e5fe2.js";import"./Scroll-f849ceca.js";import"./Select-45e6efee.js";import"./Spinner-a04ff1aa.js";import"./StopClickPropagation-30c60ee4.js";import"./Switch-dda6bb9d.js";import"./TabbedNav-81d5a752.js";import"./Tag-25df6d88.js";import{T as p}from"./Text-33c21de8.js";import"./ControlledTextInput-5e78d412.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-4d501b15.js";import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./Divider-91abf20b.js";import"./extends-98964cd2.js";import"./index.module-d15610cf.js";import"./index-ffd4619b.js";import"./index.module-e6a6661a.js";import"./index.module-45828978.js";import"./index.module-732cca0e.js";import"./WarningIcon-e965e419.js";import"./index.module-a9b21657.js";import"./motion-7df5a7b3.js";import"./NumericInput-6d8a3c99.js";import"./index.module-fa6a82d0.js";import"./index.module-8c7796ea.js";import"./Combination-ae2baabd.js";import"./index.module-f17d4a85.js";import"./Image-d41e85a0.js";import"./typography-f274f330.js";const it={title:"Components/Tabs",component:m},o=({children:i})=>t(n,{marginTop:"4",children:t(p,{variant:"normal",color:"text100",children:i})}),r={args:{defaultValue:"coins",tabs:[{value:"coins",label:"Coins",content:t(o,{children:"Coins"})},{value:"collectibles",label:"Collectibles",content:t(o,{children:"Collectibles"})},{value:"other",label:"Other",content:t(o,{children:"Other"})}]}};var e;r.parameters={...r.parameters,storySource:{source:`{
+  args: {
+    defaultValue: 'coins',
+    tabs: [{
+      value: 'coins',
+      label: 'Coins',
+      content: <Content>Coins</Content>
+    }, {
+      value: 'collectibles',
+      label: 'Collectibles',
+      content: <Content>Collectibles</Content>
+    }, {
+      value: 'other',
+      label: 'Other',
+      content: <Content>Other</Content>
+    }]
+  }
+}`,...(e=r.parameters)==null?void 0:e.storySource}};const nt=["Default"];export{r as Default,nt as __namedExportsOrder,it as default};
+//# sourceMappingURL=Tabs.stories-4657dc17.js.map
