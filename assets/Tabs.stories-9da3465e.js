907ad3def8dedcde949b0c8bc6ffb90d240cc9f2
corbanbrook <corbanbrook@gmail.com>
2023-01-25 18:55:27 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@8ca6717151a3f49cda8c380211be925e5b2feff4 🚀

---
+++ assets/Tabs.stories-9da3465e.js
@@ -0,0 +1,19 @@
+import{j as t}from"./jsx-runtime-a9c13c85.js";import"./Box-71460fdd.js";import"./Avatar-d122d221.js";import"./Badge-92d2755f.js";import"./Breadcrumbs-fb085f15.js";import"./Button-8adb6abb.js";import"./atoms.css-afddc020.js";/* empty css                              */import{C as n}from"./Card-7e8c0409.js";import"./Checkbox-5a69758f.js";import"./ControlledSwitch-fd5afc62.js";import{a as m}from"./Tooltip-44ad3acc.js";import"./index-aacf97be.js";import"./DropdownMenu-54986054.js";import"./FileInput-8faffedc.js";import"./Field-2edc6e2f.js";import"./IconButton-2fef59d9.js";import"./TokenImage-89f7d879.js";import"./Modal-98c7167a.js";import"./RadioGroup-0d95fd5a.js";import"./Scroll-f849ceca.js";import"./Select-bae0eec2.js";import"./Spinner-a04ff1aa.js";import"./StopClickPropagation-30c60ee4.js";import"./Switch-dda6bb9d.js";import"./TabbedNav-81d5a752.js";import"./Tag-e8f1b12c.js";import{T as p}from"./Text-33c21de8.js";import"./ControlledTextInput-769ec937.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-4d501b15.js";import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./Divider-91abf20b.js";import"./extends-98964cd2.js";import"./index.module-d15610cf.js";import"./index-ffd4619b.js";import"./index.module-e6a6661a.js";import"./index.module-45828978.js";import"./index.module-732cca0e.js";import"./WarningIcon-e965e419.js";import"./index.module-a9b21657.js";import"./motion-7df5a7b3.js";import"./NumericInput-c4d70df1.js";import"./index.module-3bb41f79.js";import"./index.module-8c7796ea.js";import"./Combination-78ef0d5c.js";import"./index.module-b2b96eed.js";import"./index.module-f17d4a85.js";import"./Image-d41e85a0.js";import"./index-807b81f6.js";import"./typography-f274f330.js";const mt={title:"Components/Tabs",component:m},o=({children:e})=>t(n,{marginTop:"4",children:t(p,{variant:"normal",color:"text100",children:e})}),r={args:{defaultValue:"coins",tabs:[{value:"coins",label:"Coins",content:t(o,{children:"Coins"})},{value:"collectibles",label:"Collectibles",content:t(o,{children:"Collectibles"})},{value:"other",label:"Other",content:t(o,{children:"Other"})}]}};var i;r.parameters={...r.parameters,storySource:{source:`{
+  args: {
+    defaultValue: 'coins',
+    tabs: [{
+      value: 'coins',
+      label: 'Coins',
+      content: <Content>Coins</Content>
+    }, {
+      value: 'collectibles',
+      label: 'Collectibles',
+      content: <Content>Collectibles</Content>
+    }, {
+      value: 'other',
+      label: 'Other',
+      content: <Content>Other</Content>
+    }]
+  }
+}`,...(i=r.parameters)==null?void 0:i.storySource}};const pt=["Default"];export{r as Default,pt as __namedExportsOrder,mt as default};
+//# sourceMappingURL=Tabs.stories-9da3465e.js.map
