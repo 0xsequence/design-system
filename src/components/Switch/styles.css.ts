3b1efc3d6ed687d0773e6622d3a06c4a00248bcd
Corban Riley <corbanbrook@gmail.com>
2023-01-27 14:24:24 -0500

    Adjusting styling of Switch

--- src/components/Switch/styles.css.ts
+++ src/components/Switch/styles.css.ts
@@ -1,45 +1,53 @@
 import { style } from '@vanilla-extract/css'
 
-import { vars } from '~/css'
+import { atoms, vars } from '~/css'
 
-export const root = style({
-  width: '48px',
-  height: '28px',
-  background: vars.colors.backgroundControl,
-  padding: vars.space[1],
-  borderRadius: vars.radii.circle,
-  position: 'relative',
-  cursor: 'pointer',
+export const root = style([
+  atoms({
+    background: 'backgroundControl',
+    borderRadius: 'circle',
+    padding: '1',
+    position: 'relative',
+    cursor: 'pointer',
+  }),
+  {
+    width: '48px',
+    height: '28px',
 
-  selectors: {
-    '&[data-state="checked"]': {
-      background: vars.colors.gradientPrimary,
-    },
+    selectors: {
+      '&[data-state="checked"]': {
+        background: vars.colors.gradientPrimary,
+      },
 
-    '&:disabled': {
-      cursor: 'default',
-      opacity: '0.5',
+      '&:disabled': {
+        cursor: 'default',
+        opacity: '0.5',
+      },
     },
   },
-})
+])
 
-export const thumb = style({
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  width: vars.space[5],
-  height: vars.space[5],
-  background: vars.colors.backgroundInverse,
-  borderRadius: vars.radii.circle,
-  transition: 'transform 100ms ease-out, background 100ms ease-out',
-  transform: 'translateX(0)',
-  willChange: 'transform',
+export const thumb = style([
+  atoms({
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '5',
+    height: '5',
+    background: 'white',
+    borderRadius: 'circle',
+  }),
+  {
+    transition: 'transform 100ms ease-out, background 100ms ease-out',
+    transform: 'translateX(0)',
+    willChange: 'transform',
 
-  selectors: {
-    '&[data-state="checked"]': {
-      transform: `translateX(${vars.space[5]})`,
+    selectors: {
+      '&[data-state="checked"]': {
+        transform: `translateX(${vars.space[5]})`,
+      },
+
+      [`${root}:hover:not([data-state="checked"]) &`]: {},
     },
-
-    [`${root}:hover:not([data-state="checked"]) &`]: {},
   },
-})
+])
