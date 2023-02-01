8313510b4ea029624cd2e35ad3f5dc3bef5d3709
YeungPhenom <syeung31@gmail.com>
2023-02-01 22:30:52 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@3c4cec015bf2b9a3a7a45f63fea179ba3f121d73 🚀

---
+++ assets/GradientAvatar.stories-978ea0d8.js
@@ -0,0 +1,28 @@
+import{a,F as o,j as r}from"./jsx-runtime-a9c13c85.js";import{B as t}from"./Box-fa0ac857.js";import"./Avatar-a9782bbc.js";import"./Badge-7c56dce6.js";import"./Breadcrumbs-b5b6fb20.js";import"./Button-7f73e633.js";import"./atoms-37a3518e.js";/* empty css                              */import"./Card-9fb4ff46.js";import"./Checkbox-bfa9ef44.js";import"./ControlledSwitch-a44dbaed.js";import{G as e}from"./Tooltip-7b15e768.js";import"./index-04606dac.js";import"./DropdownMenu-6a743be8.js";import"./FileInput-27fa3cd3.js";import"./Field-23124bcb.js";import"./IconButton-752759c9.js";import"./TokenImage-b5cd7f31.js";import"./Modal-e359416c.js";import"./RadioGroup-29735aca.js";import"./Scroll-99234cc4.js";import"./Select-b7673ea1.js";import"./Spinner-bf580b44.js";import"./StopClickPropagation-5f4da872.js";import"./Switch-92b74135.js";import"./TabbedNav-f5483909.js";import"./Tag-549b34e8.js";import"./Text-e0b7a790.js";import"./ControlledTextInput-052ce1d1.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-4d501b15.js";import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./Divider-9e14b5ff.js";import"./extends-98964cd2.js";import"./index.module-d15610cf.js";import"./index-ffd4619b.js";import"./index.module-e6a6661a.js";import"./index.module-45828978.js";import"./index.module-732cca0e.js";import"./WarningIcon-25714511.js";import"./index.module-a9b21657.js";import"./motion-7df5a7b3.js";import"./NumericInput-f421ce2c.js";import"./index.module-3bb41f79.js";import"./index.module-8c7796ea.js";import"./Combination-78ef0d5c.js";import"./index.module-b2b96eed.js";import"./index.module-f17d4a85.js";import"./Image-58163d4a.js";import"./index-807b81f6.js";import"./typography-f274f330.js";const xe={title:"Components/GradientAvatar",component:e},d={args:{address:"0x1234567890123456789012345678901234567890"}},s=()=>a(o,{children:[a(t,{gap:"4",children:[r(e,{address:"0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8B9",size:"xl"}),r(e,{address:"0x73F8E47dEe63002378E047fA74e6DFb24F88f496",size:"xl"}),r(e,{address:"0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECD0",size:"xl"}),r(e,{address:"0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1e",size:"xl"}),r(e,{address:"0x9aB58Bda3eeCbF391a17592ed7626727b922a18f",size:"xl"}),r(e,{address:"0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c04",size:"xl"}),r(e,{address:"0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4C",size:"xl"}),r(e,{address:"0x88cc3E896C4C178AB98333dCD29C33d2cc2102A8",size:"xl"})]}),a(t,{gap:"4",marginTop:"4",children:[r(e,{address:"0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8Bx",size:"xl"}),r(e,{address:"0x73F8E47dEe63002378E047fA74e6DFb24F88f49x",size:"xl"}),r(e,{address:"0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECDx",size:"xl"}),r(e,{address:"0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1x",size:"xl"}),r(e,{address:"0x9aB58Bda3eeCbF391a17592ed7626727b922a18x",size:"xl"}),r(e,{address:"0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c0x",size:"xl"}),r(e,{address:"0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4x",size:"xl"}),r(e,{address:"0x88cc3E896C4C178AB98333dCD29C33d2cc2102Ax",size:"xl"})]})]});var i;d.parameters={...d.parameters,storySource:{source:`{
+  args: {
+    address: '0x1234567890123456789012345678901234567890'
+  }
+}`,...(i=d.parameters)==null?void 0:i.storySource}};var x;s.parameters={...s.parameters,storySource:{source:`() => <>
+    <Box gap="4">
+      <GradientAvatar address="0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8B9" size="xl" />
+      <GradientAvatar address="0x73F8E47dEe63002378E047fA74e6DFb24F88f496" size="xl" />
+      <GradientAvatar address="0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECD0" size="xl" />
+      <GradientAvatar address="0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1e" size="xl" />
+      <GradientAvatar address="0x9aB58Bda3eeCbF391a17592ed7626727b922a18f" size="xl" />
+      <GradientAvatar address="0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c04" size="xl" />
+      <GradientAvatar address="0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4C" size="xl" />
+      <GradientAvatar address="0x88cc3E896C4C178AB98333dCD29C33d2cc2102A8" size="xl" />
+    </Box>
+
+    <Box gap="4" marginTop="4">
+      <GradientAvatar address="0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8Bx" size="xl" />
+      <GradientAvatar address="0x73F8E47dEe63002378E047fA74e6DFb24F88f49x" size="xl" />
+      <GradientAvatar address="0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECDx" size="xl" />
+      <GradientAvatar address="0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1x" size="xl" />
+      <GradientAvatar address="0x9aB58Bda3eeCbF391a17592ed7626727b922a18x" size="xl" />
+      <GradientAvatar address="0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c0x" size="xl" />
+      <GradientAvatar address="0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4x" size="xl" />
+      <GradientAvatar address="0x88cc3E896C4C178AB98333dCD29C33d2cc2102Ax" size="xl" />
+    </Box>
+  </>`,...(x=s.parameters)==null?void 0:x.storySource}};const oe=["Default","Multiple"];export{d as Default,s as Multiple,oe as __namedExportsOrder,xe as default};
+//# sourceMappingURL=GradientAvatar.stories-978ea0d8.js.map
