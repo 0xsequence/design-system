6a5b43783156277eee2b0c210fe90c3618dd0fdc
YeungPhenom <syeung31@gmail.com>
2023-02-02 00:23:20 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@f92e3fa1b9d9fbaa7545c4e1636ef05c103d0b95 🚀

---
+++ assets/TextInput.stories-7bf98a41.js
@@ -0,0 +1,44 @@
+import{j as i,a as b}from"./jsx-runtime-a9c13c85.js";import{B as g}from"./Box-fa0ac857.js";import"./Avatar-a9782bbc.js";import"./Badge-7c56dce6.js";import"./Breadcrumbs-b5b6fb20.js";import{B as l}from"./Button-7f73e633.js";import"./atoms-37a3518e.js";/* empty css                              */import"./Card-9fb4ff46.js";import"./Checkbox-bfa9ef44.js";import"./ControlledSwitch-a44dbaed.js";import"./Tooltip-3d3bfec2.js";import"./index-04606dac.js";import"./DropdownMenu-6a743be8.js";import"./FileInput-27fa3cd3.js";import"./Field-23124bcb.js";import{I as T}from"./IconButton-752759c9.js";import"./TokenImage-e750f305.js";import"./Modal-7d3ad6d5.js";import"./RadioGroup-29735aca.js";import"./Scroll-99234cc4.js";import"./Select-b7673ea1.js";import"./Spinner-bf580b44.js";import"./StopClickPropagation-5f4da872.js";import"./Switch-92b74135.js";import"./TabbedNav-f5483909.js";import"./Tag-549b34e8.js";import"./Text-e0b7a790.js";import{T as S}from"./ControlledTextInput-052ce1d1.js";import{I as d,g as I,b as x,c as f}from"./WarningIcon-25714511.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-4d501b15.js";import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./Divider-9e14b5ff.js";import"./extends-98964cd2.js";import"./index.module-d15610cf.js";import"./index-ffd4619b.js";import"./index.module-e6a6661a.js";import"./index.module-45828978.js";import"./index.module-732cca0e.js";import"./index.module-a9b21657.js";import"./motion-7df5a7b3.js";import"./NumericInput-c2442a33.js";import"./index.module-3bb41f79.js";import"./index.module-8c7796ea.js";import"./Combination-78ef0d5c.js";import"./index.module-b2b96eed.js";import"./index.module-f17d4a85.js";import"./Image-4f8f7be0.js";import"./index-807b81f6.js";import"./typography-f274f330.js";const xe={title:"Forms/TextInput",component:S},e={args:{borderRadius:"md",label:"This the label",description:"This is the description",placeholder:"This is the placeholder",labelLocation:"top"}},o={args:{label:"This the label",placeholder:"This is the placeholder",leftIcon:d}},r={args:{label:"This the label",placeholder:"This is the placeholder",rightIcon:I}},t={args:{label:"This the label",placeholder:"This is the placeholder",leftIcon:d,rightIcon:I}},s={args:{placeholder:"Search for coins or collectibles",leftIcon:x,controls:i(T,{icon:f,size:"xs"})}},a={args:{label:"This the label",placeholder:"This is the placeholder",controls:b(g,{gap:"2",children:[i(l,{label:"Max",size:"xs",shape:"square"}),i(l,{label:"Paste",size:"xs",shape:"square"})]})}};var p;e.parameters={...e.parameters,storySource:{source:`{
+  args: {
+    borderRadius: 'md',
+    label: 'This the label',
+    description: 'This is the description',
+    placeholder: 'This is the placeholder',
+    labelLocation: 'top'
+  }
+}`,...(p=e.parameters)==null?void 0:p.storySource}};var c;o.parameters={...o.parameters,storySource:{source:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    leftIcon: ScanIcon
+  }
+}`,...(c=o.parameters)==null?void 0:c.storySource}};var h;r.parameters={...r.parameters,storySource:{source:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    rightIcon: ArrowRightIcon
+  }
+}`,...(h=r.parameters)==null?void 0:h.storySource}};var n;t.parameters={...t.parameters,storySource:{source:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    leftIcon: ScanIcon,
+    rightIcon: ArrowRightIcon
+  }
+}`,...(n=t.parameters)==null?void 0:n.storySource}};var m;s.parameters={...s.parameters,storySource:{source:`{
+  args: {
+    placeholder: 'Search for coins or collectibles',
+    leftIcon: SearchIcon,
+    controls: <IconButton icon={CloseIcon} size="xs" />
+  }
+}`,...(m=s.parameters)==null?void 0:m.storySource}};var u;a.parameters={...a.parameters,storySource:{source:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    controls: <Box gap="2">
+        <Button label="Max" size="xs" shape="square" />
+        <Button label="Paste" size="xs" shape="square" />
+      </Box>
+  }
+}`,...(u=a.parameters)==null?void 0:u.storySource}};const fe=["Default","WithLeftIcon","WithRightIcon","WithBothIcons","SearchInput","WithComplexControls"];export{e as Default,s as SearchInput,t as WithBothIcons,a as WithComplexControls,o as WithLeftIcon,r as WithRightIcon,fe as __namedExportsOrder,xe as default};
+//# sourceMappingURL=TextInput.stories-7bf98a41.js.map
