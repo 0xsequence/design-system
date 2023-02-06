d5ee5f084c75f57d15c310eaaca31c46643aed6d
YeungPhenom <syeung31@gmail.com>
2023-02-06 19:03:21 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@5e446cdb8074802ac7cc7acaf919abe620351362 🚀

--- assets/Checkbox.stories-5d0cf7d2.js
+++
@@ -1,9 +0,0 @@
-import{C as t}from"./Checkbox-bfa9ef44.js";import"./jsx-runtime-a9c13c85.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./extends-98964cd2.js";import"./index.module-d15610cf.js";import"./index-ffd4619b.js";import"./index.module-e6a6661a.js";import"./index.module-45828978.js";import"./index.module-732cca0e.js";import"./Field-23124bcb.js";import"./Box-fa0ac857.js";import"./atoms-37a3518e.js";import"./Text-e0b7a790.js";import"./typography-f274f330.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./WarningIcon-25714511.js";const _={title:"Forms/Checkbox",component:t},o={args:{label:"This is the label (CheckmarkIcon is missing)",labelLocation:"right",size:"sm",disabled:!1}};var r;o.parameters={...o.parameters,storySource:{source:`{
-  args: {
-    label: 'This is the label (CheckmarkIcon is missing)',
-    labelLocation: 'right',
-    size: 'sm',
-    disabled: false
-  }
-}`,...(r=o.parameters)==null?void 0:r.storySource}};const y=["Default"];export{o as Default,y as __namedExportsOrder,_ as default};
-//# sourceMappingURL=Checkbox.stories-5d0cf7d2.js.map
