c64fd8d21be2b3fd162f4c2fdbd0700897218a44
corbanbrook <corbanbrook@gmail.com>
2023-02-15 20:55:27 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@8012eb4f794f6b8be234320a73092268bc42b26d 🚀

---
+++ assets/GradientAvatar.stories-e056d6b7.js
@@ -0,0 +1,28 @@
+import{a,F as o,j as r}from"./jsx-runtime-a9c13c85.js";import{B as t}from"./Box-fa0ac857.js";import"./Avatar-a9782bbc.js";import"./Badge-7c56dce6.js";import"./Breadcrumbs-f88498c8.js";import"./Button-7f73e633.js";import"./Card-9fb4ff46.js";import"./Checkbox-4e648058.js";import"./ControlledSwitch-93d9b314.js";import{G as e}from"./Tooltip-43c8629b.js";import"./index-04606dac.js";import"./DropdownMenu-975bbd37.js";import"./FileInput-5fed61ef.js";import"./Field-216ed884.js";import"./IconButton-5d85b89e.js";import"./TokenImage-c0e1a762.js";import"./Modal-53b77cd3.js";import"./RadioGroup-6cdc93eb.js";import"./Scroll-99234cc4.js";import"./Select-a3f7e943.js";import"./Spinner-d7e61b37.js";import"./StopClickPropagation-5f4da872.js";import"./Switch-e9b3999e.js";import"./TabbedNav-207f90cf.js";import"./Tag-549b34e8.js";import"./Text-e0b7a790.js";import"./ControlledTextInput-89220347.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./atoms-37a3518e.js";import"./index-4d501b15.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./Divider-9e14b5ff.js";import"./extends-98964cd2.js";import"./index.module-1b91e82f.js";import"./index-ffd4619b.js";import"./index.module-99b258bb.js";import"./index.module-e6a6661a.js";import"./index.module-f6547125.js";import"./index.module-5cafe27c.js";import"./WarningIcon-25714511.js";import"./index.module-cc7c4bf6.js";import"./motion-7df5a7b3.js";import"./NumericInput-c5c7f111.js";import"./Progress-cc40f513.js";import"./index.module-9fd418d9.js";import"./index.module-207f3122.js";import"./Combination-ff871947.js";import"./index.module-6743eeab.js";import"./index.module-05556e0d.js";import"./Image-58163d4a.js";import"./index-807b81f6.js";import"./typography-f274f330.js";const pe={title:"Components/GradientAvatar",component:e},d={args:{address:"0x1234567890123456789012345678901234567890"}},s=()=>a(o,{children:[a(t,{gap:"4",children:[r(e,{address:"0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8B9",size:"xl"}),r(e,{address:"0x73F8E47dEe63002378E047fA74e6DFb24F88f496",size:"xl"}),r(e,{address:"0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECD0",size:"xl"}),r(e,{address:"0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1e",size:"xl"}),r(e,{address:"0x9aB58Bda3eeCbF391a17592ed7626727b922a18f",size:"xl"}),r(e,{address:"0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c04",size:"xl"}),r(e,{address:"0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4C",size:"xl"}),r(e,{address:"0x88cc3E896C4C178AB98333dCD29C33d2cc2102A8",size:"xl"})]}),a(t,{gap:"4",marginTop:"4",children:[r(e,{address:"0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8Bx",size:"xl"}),r(e,{address:"0x73F8E47dEe63002378E047fA74e6DFb24F88f49x",size:"xl"}),r(e,{address:"0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECDx",size:"xl"}),r(e,{address:"0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1x",size:"xl"}),r(e,{address:"0x9aB58Bda3eeCbF391a17592ed7626727b922a18x",size:"xl"}),r(e,{address:"0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c0x",size:"xl"}),r(e,{address:"0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4x",size:"xl"}),r(e,{address:"0x88cc3E896C4C178AB98333dCD29C33d2cc2102Ax",size:"xl"})]})]});var i;d.parameters={...d.parameters,storySource:{source:`{
+  args: {
+    address: '0x1234567890123456789012345678901234567890'
+  }
+}`,...(i=d.parameters)==null?void 0:i.storySource}};var x;s.parameters={...s.parameters,storySource:{source:`() => <>
+    <Box gap="4">
+      <GradientAvatar address="0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8B9" size="xl" />
+      <GradientAvatar address="0x73F8E47dEe63002378E047fA74e6DFb24F88f496" size="xl" />
+      <GradientAvatar address="0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECD0" size="xl" />
+      <GradientAvatar address="0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1e" size="xl" />
+      <GradientAvatar address="0x9aB58Bda3eeCbF391a17592ed7626727b922a18f" size="xl" />
+      <GradientAvatar address="0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c04" size="xl" />
+      <GradientAvatar address="0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4C" size="xl" />
+      <GradientAvatar address="0x88cc3E896C4C178AB98333dCD29C33d2cc2102A8" size="xl" />
+    </Box>
+
+    <Box gap="4" marginTop="4">
+      <GradientAvatar address="0x8e3E38fe7367dd3b52D1e281E4e8400447C8d8Bx" size="xl" />
+      <GradientAvatar address="0x73F8E47dEe63002378E047fA74e6DFb24F88f49x" size="xl" />
+      <GradientAvatar address="0x9Cb019C2FfcD9E9BB83e269625a170179Ff3ECDx" size="xl" />
+      <GradientAvatar address="0x9b436D9599FAc498e1e0Ab4D48cDFC72922a6F1x" size="xl" />
+      <GradientAvatar address="0x9aB58Bda3eeCbF391a17592ed7626727b922a18x" size="xl" />
+      <GradientAvatar address="0x468E8e29F6cfb0F6b7ff10ec6A1AB516ec849c0x" size="xl" />
+      <GradientAvatar address="0x450cB9fbB2D44d166AACA1f6cDb1dBd9Ff168e4x" size="xl" />
+      <GradientAvatar address="0x88cc3E896C4C178AB98333dCD29C33d2cc2102Ax" size="xl" />
+    </Box>
+  </>`,...(x=s.parameters)==null?void 0:x.storySource}};const me=["Default","Multiple"];export{d as Default,s as Multiple,me as __namedExportsOrder,pe as default};
+//# sourceMappingURL=GradientAvatar.stories-e056d6b7.js.map
---
+++ assets/Spinner.stories-152d6a75.js
@@ -0,0 +1,11 @@
+import{S as t}from"./Spinner-d7e61b37.js";import"./jsx-runtime-a9c13c85.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./Box-fa0ac857.js";import"./atoms-37a3518e.js";import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";const d={title:"Components/Spinner",component:t},r={args:{size:"md"}},e={args:{size:"md",color:"warning"}};var n;r.parameters={...r.parameters,storySource:{source:`{
+  args: {
+    size: 'md'
+  }
+}`,...(n=r.parameters)==null?void 0:n.storySource}};var o;e.parameters={...e.parameters,storySource:{source:`{
+  args: {
+    size: 'md',
+    color: 'warning'
+  }
+}`,...(o=e.parameters)==null?void 0:o.storySource}};const u=["Default","PendingSpinner"];export{r as Default,e as PendingSpinner,u as __namedExportsOrder,d as default};
+//# sourceMappingURL=Spinner.stories-152d6a75.js.map
