65f59c4299b7942b99dfa2664db7d57f5d3e32a1
corbanbrook <corbanbrook@gmail.com>
2023-02-16 20:29:22 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@07dd7ef6926ee40210d7a84433ef41d09c0258e3 🚀

---
+++ assets/TokenImage.stories-8f915a95.js
@@ -0,0 +1,24 @@
+import{T as c}from"./TokenImage-1d9cb2b4.js";import"./jsx-runtime-a9c13c85.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./Box-62a4f41e.js";import"./atoms-b77c2037.js";import"./Image-13c67576.js";import"./motion-7df5a7b3.js";import"./Text-3ada69fe.js";import"./typography-f274f330.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";const y={title:"Components/TokenImage",component:c},e={args:{borderRadius:"circle",size:"lg",src:"https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912"}},r={args:{size:"lg",symbol:"SCAM"}};var s,o,t;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    borderRadius: 'circle',
+    size: 'lg',
+    src: 'https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912'
+  }
+}`,...(t=(o=e.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};var a,n,i;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    size: 'lg',
+    symbol: 'SCAM'
+  }
+}`,...(i=(n=r.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};e.parameters={storySource:{source:`{
+  args: {
+    borderRadius: 'circle',
+    size: 'lg',
+    src: 'https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912'
+  }
+}`},...e.parameters};r.parameters={storySource:{source:`{
+  args: {
+    size: 'lg',
+    symbol: 'SCAM'
+  }
+}`},...r.parameters};const C=["WithImage","WithoutImage"];export{e as WithImage,r as WithoutImage,C as __namedExportsOrder,y as default};
+//# sourceMappingURL=TokenImage.stories-8f915a95.js.map
