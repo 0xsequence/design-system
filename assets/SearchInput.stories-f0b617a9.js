dea9c053bea5cb2dd5b4c6cbb688e54428436814
YeungPhenom <syeung31@gmail.com>
2023-03-17 20:15:30 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@0ab56e1860182ee181c4ddff33311afaa3e22577 🚀

---
+++ assets/SearchInput.stories-f0b617a9.js
@@ -0,0 +1,16 @@
+import{j as n}from"./jsx-runtime-a9c13c85.js";import{r as l}from"./index-d8ade8ea.js";import{S as e}from"./Tooltip-df277a00.js";import"./_commonjsHelpers-042e6b4d.js";import"./Box-e37d6a6c.js";import"./atoms-2c07736f.js";import"./Avatar-f0cafcdb.js";import"./index-4d501b15.js";import"./Badge-da925616.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm-d30eb010.js";import"./Breadcrumbs-46a00736.js";import"./index-050725ba.js";import"./Divider-782c6dda.js";import"./Text-0105fdd6.js";import"./typography-f274f330.js";import"./Button-f9a698d9.js";import"./Card-8265b3f9.js";import"./Checkbox-f27d05e6.js";import"./extends-98964cd2.js";import"./index.module-1b91e82f.js";import"./index-ffd4619b.js";import"./index.module-99b258bb.js";import"./index.module-e6a6661a.js";import"./index.module-f6547125.js";import"./index.module-5cafe27c.js";import"./Field-ec8de895.js";import"./WarningIcon-841a4d7b.js";import"./ControlledSwitch-b9347f27.js";import"./ControlledTextInput-5faa9406.js";import"./FileInput-4afbfc88.js";import"./IconButton-96049cef.js";import"./RadioGroup-50c695a1.js";import"./index.module-9fd418d9.js";import"./index.module-207f3122.js";import"./index.module-cc7c4bf6.js";import"./Select-0410c48f.js";import"./Combination-ff871947.js";import"./index.module-6743eeab.js";import"./index-807b81f6.js";import"./Switch-86aa03bb.js";import"./motion-7df5a7b3.js";import"./Modal-0dcc502b.js";import"./index.module-05556e0d.js";import"./Scroll-4e4d768f.js";import"./DropdownMenu-f0ea02db.js";import"./TokenImage-cbe20ce0.js";import"./Image-c5e8b89d.js";import"./NumericInput-d58de15b.js";import"./Placeholder-70122d5d.js";import"./Progress-54571e01.js";import"./Spinner-211cacec.js";import"./StopClickPropagation-1fc9d2f2.js";import"./TabbedNav-c86eb4f0.js";import"./Tag-3c799d3f.js";const ur={title:"Forms/SearchInput",component:e},c=i=>{const[m,a]=l.useState("");return n(e,{as:"input",id:"myInput",value:m,onChange:s=>a(s.target.value),...i})},r={render:c,args:{label:"Search",labelLocation:"top",placeholder:"This is the placeholder"}};var t,o,p;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: StoryWrapper,
+  args: {
+    label: 'Search',
+    labelLocation: 'top',
+    placeholder: 'This is the placeholder'
+  }
+}`,...(p=(o=r.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};r.parameters={storySource:{source:`{
+  render: StoryWrapper,
+  args: {
+    label: 'Search',
+    labelLocation: 'top',
+    placeholder: 'This is the placeholder'
+  }
+}`},...r.parameters};const dr=["Default"];export{r as Default,dr as __namedExportsOrder,ur as default};
+//# sourceMappingURL=SearchInput.stories-f0b617a9.js.map
