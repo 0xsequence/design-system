5d758c66cac38ad6cea790c0106c9f20294f26e0
corbanbrook <corbanbrook@gmail.com>
2023-04-05 15:07:08 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@3a010a922f6a4a47894949c313444b9c43c6d96e 🚀

---
+++ assets/Introduction.stories-00eaa075.js
@@ -0,0 +1,40 @@
+import{M as c}from"./chunk-PCJTTTQV-b865c73a.js";import"./chunk-R4NKYYJA-96bb58e6.js";import{j as n,a as r,F as a}from"./jsx-runtime-a9c13c85.js";import{u as s}from"./index-f3daabb9.js";import"./iframe-50a0c084.js";import"../sb-preview/runtime.mjs";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-2b7ca8d1.js";import"./index-6cf4ba34.js";import"./index-9c518322.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./extends-9cd95192.js";import"./index-356e4a49.js";function d(t={}){const{wrapper:i}=Object.assign({},s(),t.components);return i?n(i,{...t,children:n(l,{})}):l();function l(){const e=Object.assign({h1:"h1",p:"p",a:"a",h3:"h3",pre:"pre",code:"code",ul:"ul",li:"li",strong:"strong"},s(),t.components);return r(a,{children:[n(c,{title:"Introduction"}),`
+`,n(e.h1,{id:"sequence-design-system",children:"Sequence Design System"}),`
+`,n(e.p,{children:"Sequence Design System is a reusable component library uses across the Sequence product suite."}),`
+`,r(e.p,{children:["Components are written in ",n(e.a,{href:"https://reactjs.org/",target:"_blank",rel:"nofollow noopener noreferrer",children:"React"})," with ",n(e.a,{href:"https://vanilla-extract.style/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Vanilla Extract"}),", and its stories are written in ",n(e.a,{href:"https://medium.com/storybookjs/component-story-format-66f4c32366df",target:"_blank",rel:"nofollow noopener noreferrer",children:"Component Story Format"}),"."]}),`
+`,n(e.h3,{id:"install",children:"Install"}),`
+`,n(e.pre,{children:n(e.code,{children:`pnpm add @0xsequence/design-system
+`})}),`
+`,n(e.h3,{id:"use",children:"Use"}),`
+`,n(e.p,{children:"Import components you want into your UI"}),`
+`,n(e.p,{children:n(e.code,{children:"import { Button, Badge } from '@0xsequence/design-system'"})}),`
+`,n(e.p,{children:"and use them like so"}),`
+`,n(e.pre,{children:n(e.code,{children:`const example = () => (
+
+<div>
+  <Button appearance="primary" onClick={() => 0}>
+    Do something
+  </Button>
+  <Badge onClick={this.clear}>Cool</Badge>
+</div>
+)
+
+`})}),`
+`,n(e.h3,{id:"run-and-develop-locally",children:"Run and Develop Locally"}),`
+`,r(e.p,{children:["Clone the ",n(e.a,{href:"https://github.com/0xsequence/design-system",target:"_blank",rel:"nofollow noopener noreferrer",children:"Sequence Design System GitHub Project"})," then start Storybook."]}),`
+`,n(e.pre,{children:n(e.code,{children:`pnpm install && pnpm storybook
+`})}),`
+`,n(e.h3,{id:"used-by",children:"Used by"}),`
+`,r(e.ul,{children:[`
+`,n(e.li,{children:n(e.a,{href:"https://sequence.app/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Sequence Wallet"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://sequence.dev/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Sequence Console"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://status.sequence.info/",target:"_blank",rel:"nofollow noopener noreferrer",children:"Sequence Status"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://github.com/0xsequence/demo-dapp",target:"_blank",rel:"nofollow noopener noreferrer",children:"Sequence Demo Dapp Github Project"})}),`
+`]}),`
+`,n(e.p,{children:"Note: this package is not used in Storybook's UI, but the visual design is identical."}),`
+`,n(e.h3,{id:"resources",children:n(e.strong,{children:"Resources"})}),`
+`,r(e.ul,{children:[`
+`,n(e.li,{children:n(e.a,{href:"https://storybook.js.org/docs/react/get-started/introduction",target:"_blank",rel:"nofollow noopener noreferrer",children:"Storybook Documentation"})}),`
+`,n(e.li,{children:n(e.a,{href:"https://github.com/0xsequence/design-system",target:"_blank",rel:"nofollow noopener noreferrer",children:"GitHub Repository"})}),`
+`]})]})}}const p=()=>{throw new Error("Docs-only story")};p.parameters={docsOnly:!0};const o={title:"Introduction",tags:["stories-mdx"],includeStories:["__page"]};o.parameters=o.parameters||{};o.parameters.docs={...o.parameters.docs||{},page:d};const C=["__page"];export{C as __namedExportsOrder,p as __page,o as default};
+//# sourceMappingURL=Introduction.stories-00eaa075.js.map
---
+++ assets/TextInput.stories-4d604e6d.js
@@ -0,0 +1,44 @@
+import{j as i,a as v}from"./jsx-runtime-a9c13c85.js";import{B as L}from"./Box-9e9f1e0d.js";import"./Avatar-09f66520.js";import"./Badge-56d8040a.js";import"./Breadcrumbs-9e3eb3d1.js";import{B as l}from"./Button-c055484d.js";import"./Card-4eb0dff8.js";import"./Checkbox-33ad8c92.js";import"./ControlledSwitch-3dd65d39.js";import"./Tooltip-b71c0a1b.js";import"./index-61ebbbb1.js";import"./DropdownMenu-a3ce6134.js";import"./FileInput-b17550bb.js";import"./Field-7c6ec1ee.js";import{I as j}from"./IconButton-fd570a0d.js";import"./TokenImage-bff856d6.js";import"./Modal-a7cd29b1.js";import"./Placeholder-fd0c057f.js";import"./RadioGroup-aea90318.js";import"./Scroll-37fd8e55.js";import"./Select-8bd7f337.js";import"./Spinner-86a32ff1.js";import"./StopClickPropagation-aca2e071.js";import"./Switch-57dc6d54.js";import"./TabbedNav-bb1b6f85.js";import"./Tag-fb056a1d.js";import"./Text-755617cc.js";import"./Toast-3aa22492.js";import{T as w}from"./ControlledTextInput-7e733c7f.js";import{G as C,b as q,H as A,i as _}from"./WarningIcon-60d06b1a.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./atoms-53256ec3.js";import"./index-4d501b15.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm-bf276de8.js";import"./Divider-0053639b.js";import"./extends-98964cd2.js";import"./index.module-4456f0d6.js";import"./extends-9cd95192.js";import"./index-6cf4ba34.js";import"./index.module-99b258bb.js";import"./index.module-e6a6661a.js";import"./index.module-f6547125.js";import"./index.module-cacec922.js";import"./index.module-cc7c4bf6.js";import"./motion-bd85eac6.js";import"./NumericInput-c38eb37a.js";import"./Progress-6fe01423.js";import"./index.module-880e327b.js";import"./index.module-a3790cd2.js";import"./index.module-1e59790f.js";import"./index.module-8c8c5ddb.js";import"./index.module-2789a3d5.js";import"./index.module-ba9cd207.js";import"./Combination-888d27bc.js";import"./index.module-5c3234a3.js";import"./Image-f63e47ac.js";import"./index-807b81f6.js";import"./typography-f274f330.js";const Oo={title:"Forms/TextInput",component:w},o={args:{borderRadius:"md",label:"This the label",description:"This is the description",placeholder:"This is the placeholder",labelLocation:"top"}},r={args:{label:"This the label",placeholder:"This is the placeholder",leftIcon:C}},e={args:{label:"This the label",placeholder:"This is the placeholder",rightIcon:q}},t={args:{label:"This the label",placeholder:"This is the placeholder",leftIcon:C,rightIcon:q}},s={args:{placeholder:"Search for coins or collectibles",leftIcon:A,controls:i(j,{icon:_,size:"xs"})}},a={args:{label:"This the label",placeholder:"This is the placeholder",controls:v(L,{gap:"2",children:[i(l,{label:"Max",size:"xs",shape:"square"}),i(l,{label:"Paste",size:"xs",shape:"square"})]})}};var p,c,n;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    borderRadius: 'md',
+    label: 'This the label',
+    description: 'This is the description',
+    placeholder: 'This is the placeholder',
+    labelLocation: 'top'
+  }
+}`,...(n=(c=o.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};var m,h,d;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    leftIcon: ScanIcon
+  }
+}`,...(d=(h=r.parameters)==null?void 0:h.docs)==null?void 0:d.source}}};var u,I,g;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    rightIcon: ArrowRightIcon
+  }
+}`,...(g=(I=e.parameters)==null?void 0:I.docs)==null?void 0:g.source}}};var b,T,x;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    leftIcon: ScanIcon,
+    rightIcon: ArrowRightIcon
+  }
+}`,...(x=(T=t.parameters)==null?void 0:T.docs)==null?void 0:x.source}}};var f,S,B;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Search for coins or collectibles',
+    leftIcon: SearchIcon,
+    controls: <IconButton icon={CloseIcon} size="xs" />
+  }
+}`,...(B=(S=s.parameters)==null?void 0:S.docs)==null?void 0:B.source}}};var W,R,z;a.parameters={...a.parameters,docs:{...(W=a.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    controls: <Box gap="2">
+        <Button label="Max" size="xs" shape="square" />
+        <Button label="Paste" size="xs" shape="square" />
+      </Box>
+  }
+}`,...(z=(R=a.parameters)==null?void 0:R.docs)==null?void 0:z.source}}};const ko=["Default","WithLeftIcon","WithRightIcon","WithBothIcons","SearchInput","WithComplexControls"];export{o as Default,s as SearchInput,t as WithBothIcons,a as WithComplexControls,r as WithLeftIcon,e as WithRightIcon,ko as __namedExportsOrder,Oo as default};
+//# sourceMappingURL=TextInput.stories-4d604e6d.js.map
