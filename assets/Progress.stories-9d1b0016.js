9b4032a786586e42a46485afbd22323e98cf4631
corbanbrook <corbanbrook@gmail.com>
2023-04-06 14:48:36 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@742b3cd0dc92a7536c35d7325054147db14baa68 🚀

--- assets/Progress.stories-9d1b0016.js
+++ assets/Progress.stories-9d1b0016.js
@@ -1,4 +1,4 @@
-import{P as s}from"./Progress-6fe01423.js";import"./jsx-runtime-a9c13c85.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./extends-9cd95192.js";import"./index.module-4456f0d6.js";import"./extends-98964cd2.js";import"./index-6cf4ba34.js";import"./Box-9e9f1e0d.js";import"./atoms-53256ec3.js";const f={title:"Components/Progress",component:s,argTypes:{value:{control:{type:"range",min:0,max:1,step:.1}}}},r={args:{value:.5}};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+import{P as s}from"./Progress-6fe01423.js";import"./jsx-runtime-a9c13c85.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./extends-98964cd2.js";import"./index.module-4456f0d6.js";import"./extends-9cd95192.js";import"./index-6cf4ba34.js";import"./Box-9e9f1e0d.js";import"./atoms-53256ec3.js";const f={title:"Components/Progress",component:s,argTypes:{value:{control:{type:"range",min:0,max:1,step:.1}}}},r={args:{value:.5}};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
   args: {
     value: 0.5
   }
