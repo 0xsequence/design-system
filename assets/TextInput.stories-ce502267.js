c31b0f54683124e93ce14c893debb53805e5dd92
corbanbrook <corbanbrook@gmail.com>
2023-04-11 19:07:06 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@a9907f7261ca28c9f1e20ab8fc9c085ed41eb62b 🚀

---
+++ assets/TextInput.stories-ce502267.js
@@ -0,0 +1,44 @@
+import{j as i,a as q}from"./jsx-runtime-a9c13c85.js";import{B as v}from"./Box-9e9f1e0d.js";import"./Avatar-09f66520.js";import"./Badge-56d8040a.js";import"./Breadcrumbs-9e3eb3d1.js";import{B as l}from"./Button-c055484d.js";import"./Card-4eb0dff8.js";import"./Checkbox-b805f1d3.js";import"./ControlledSwitch-0aabd28e.js";import"./Tooltip-0df2e419.js";import"./index-61ebbbb1.js";import"./DropdownMenu-2b442a4c.js";import"./FileInput-4e230ffd.js";import"./Field-4954da28.js";import{I as j}from"./IconButton-fd570a0d.js";import"./TokenImage-bff856d6.js";import"./Modal-305a611c.js";import"./Placeholder-fd0c057f.js";import"./RadioGroup-72980a2e.js";import"./Scroll-37fd8e55.js";import"./Select-b25de084.js";import"./Spinner-86a32ff1.js";import"./StopClickPropagation-aca2e071.js";import"./Switch-c769e670.js";import"./TabbedNav-bb1b6f85.js";import"./Tag-fb056a1d.js";import"./Text-755617cc.js";import"./Toast-b5d64200.js";import{T as w}from"./ControlledTextInput-ecf2817d.js";import{K as C,b as L,L as A,i as _}from"./WarningIcon-5b052aef.js";import"./index-d8ade8ea.js";import"./_commonjsHelpers-042e6b4d.js";import"./atoms-53256ec3.js";import"./index-4d501b15.js";/* empty css                              */import"./vanilla-extract-recipes-createRuntimeFn.esm-bf276de8.js";import"./Divider-0053639b.js";import"./extends-98964cd2.js";import"./index.module-4456f0d6.js";import"./extends-9cd95192.js";import"./index-6cf4ba34.js";import"./index.module-99b258bb.js";import"./index.module-e6a6661a.js";import"./index.module-f6547125.js";import"./index.module-cacec922.js";import"./index.module-cc7c4bf6.js";import"./motion-bd85eac6.js";import"./index-e15af79d.js";import"./NumericInput-72abadd7.js";import"./Progress-6fe01423.js";import"./index.module-880e327b.js";import"./index.module-a3790cd2.js";import"./index.module-1e59790f.js";import"./index.module-f536abc8.js";import"./index.module-2789a3d5.js";import"./index.module-ba9cd207.js";import"./Combination-888d27bc.js";import"./index.module-5c3234a3.js";import"./Image-f63e47ac.js";import"./index-807b81f6.js";import"./typography-f274f330.js";const yo={title:"Forms/TextInput",component:w},o={args:{borderRadius:"md",label:"This the label",description:"This is the description",placeholder:"This is the placeholder",labelLocation:"top"}},r={args:{label:"This the label",placeholder:"This is the placeholder",leftIcon:C}},e={args:{label:"This the label",placeholder:"This is the placeholder",rightIcon:L}},t={args:{label:"This the label",placeholder:"This is the placeholder",leftIcon:C,rightIcon:L}},s={args:{placeholder:"Search for coins or collectibles",leftIcon:A,controls:i(j,{icon:_,size:"xs"})}},a={args:{label:"This the label",placeholder:"This is the placeholder",controls:q(v,{gap:"2",children:[i(l,{label:"Max",size:"xs",shape:"square"}),i(l,{label:"Paste",size:"xs",shape:"square"})]})}};var p,c,n;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    borderRadius: 'md',
+    label: 'This the label',
+    description: 'This is the description',
+    placeholder: 'This is the placeholder',
+    labelLocation: 'top'
+  }
+}`,...(n=(c=o.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};var m,h,d;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    leftIcon: ScanIcon
+  }
+}`,...(d=(h=r.parameters)==null?void 0:h.docs)==null?void 0:d.source}}};var u,I,g;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    rightIcon: ArrowRightIcon
+  }
+}`,...(g=(I=e.parameters)==null?void 0:I.docs)==null?void 0:g.source}}};var b,T,x;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    leftIcon: ScanIcon,
+    rightIcon: ArrowRightIcon
+  }
+}`,...(x=(T=t.parameters)==null?void 0:T.docs)==null?void 0:x.source}}};var f,S,B;s.parameters={...s.parameters,docs:{...(f=s.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Search for coins or collectibles',
+    leftIcon: SearchIcon,
+    controls: <IconButton icon={CloseIcon} size="xs" />
+  }
+}`,...(B=(S=s.parameters)==null?void 0:S.docs)==null?void 0:B.source}}};var W,R,z;a.parameters={...a.parameters,docs:{...(W=a.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  args: {
+    label: 'This the label',
+    placeholder: 'This is the placeholder',
+    controls: <Box gap="2">
+        <Button label="Max" size="xs" shape="square" />
+        <Button label="Paste" size="xs" shape="square" />
+      </Box>
+  }
+}`,...(z=(R=a.parameters)==null?void 0:R.docs)==null?void 0:z.source}}};const Go=["Default","WithLeftIcon","WithRightIcon","WithBothIcons","SearchInput","WithComplexControls"];export{o as Default,s as SearchInput,t as WithBothIcons,a as WithComplexControls,r as WithLeftIcon,e as WithRightIcon,Go as __namedExportsOrder,yo as default};
+//# sourceMappingURL=TextInput.stories-ce502267.js.map
