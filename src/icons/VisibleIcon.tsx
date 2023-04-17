7456a9d9a9d65f1235ec589697d9159071c52a94
Corban Riley <corbanbrook@gmail.com>
2023-04-17 11:09:13 -0400

    Updating icons

---
+++ src/icons/VisibleIcon.tsx
@@ -0,0 +1,40 @@
+// Auto-generated file created by svgr-cli source /src/icons/template.js
+// Run pnpm build:icons to update
+import { SVGProps } from 'react'
+
+import { Box } from '~/components/Box'
+
+import { iconVariants } from './styles.css'
+import { IconProps } from './types'
+
+const Svg = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3.103 10.154C4.59 12.077 7.11 13.366 10 13.366c2.89 0 5.41-1.29 6.897-3.212C15.41 8.232 12.89 6.942 10 6.942c-2.89 0-5.41 1.29-6.897 3.212Zm-1.548-.416C3.285 7.145 6.436 5.442 10 5.442s6.716 1.703 8.445 4.296l.277.416-.277.416c-1.73 2.594-4.881 4.296-8.445 4.296s-6.716-1.702-8.445-4.296l-.277-.416.277-.416Z"
+      fill="currentColor"
+    />
+    <path
+      d="M12.386 10.154a2.386 2.386 0 1 1-4.772 0 2.386 2.386 0 0 1 4.772 0Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+
+const SvgVisibleIcon = ({ size = 'sm', ...props }: IconProps) => (
+  <Box
+    as={Svg}
+    className={iconVariants({
+      size,
+    })}
+    {...props}
+  />
+)
+
+export default SvgVisibleIcon
