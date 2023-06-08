73e8f4fb19496611c86cb64392cb4625a7a44313
corbanbrook <corbanbrook@gmail.com>
2023-06-08 18:15:33 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@4d9efdf8b8929f8fc3c8c60b31a66712cebac718 🚀

---
+++ assets/preview-9cf07340.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-acfcbfd1.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-7e6a0cdc.js"),["./DocsRenderer-EYKKDMVH-7e6a0cdc.js","./chunk-PCJTTTQV-aad2b9e0.js","./iframe-acfcbfd1.js","./index-32f9f3e1.js","./index-0b2830d0.js","./index-81ed4395.js","./chunk-4NMOSTKD-82fbb5f2.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-9cf07340.js.map
