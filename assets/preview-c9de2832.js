463c70d804e6fc069699f146d2e906098a5f6479
YeungPhenom <syeung31@gmail.com>
2023-07-15 04:41:08 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@60aca24335da1327cd612cbd33893668cdd06f3d 🚀

--- assets/preview-c9de2832.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-3aa086cf.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-EYKKDMVH-c841d065.js"),["./DocsRenderer-EYKKDMVH-c841d065.js","./chunk-PCJTTTQV-febb1b5f.js","./iframe-3aa086cf.js","./index-32f9f3e1.js","./index-7e85ca20.js","./index-81ed4395.js","./chunk-4NMOSTKD-82fbb5f2.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-c9de2832.js.map
