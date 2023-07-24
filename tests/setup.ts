779ae01c4f89d6e8701c4c7d3cc17b1f93e043c7
Corban Riley <corbanbrook@gmail.com>
2023-07-24 15:49:44 -0400

    Removing jest config

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,6 +1,3 @@
-// import '@testing-library/jest-dom'
-// import '@testing-library/jest-dom/extend-expect'
-
 import matchers from '@testing-library/jest-dom/matchers'
 import { cleanup } from '@testing-library/react'
 import { expect, afterEach } from 'vitest'
