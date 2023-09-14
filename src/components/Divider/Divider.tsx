eb8379e67018fefa50397fbe1add1133dda304c5
Corban Riley <corbanbrook@gmail.com>
2023-09-14 18:29:12 -0400

    Changing default Divider color to be muted

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -3,7 +3,7 @@ import { Box, BoxProps } from '~/components/Box'
 type DividerProps = BoxProps
 
 export const Divider = (props: DividerProps) => {
-  const { color = 'borderNormal', marginY = '4', ...rest } = props
+  const { color = 'backgroundMuted', marginY = '4', ...rest } = props
 
   return (
     <Box
