7312272073fc240e949015d8f1806e880f179f94
corbanbrook <corbanbrook@gmail.com>
2023-11-28 19:59:45 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@88b05b7479129bb56e69c5ffaa388b3a9de7fd0a 🚀

--- assets/Divider.stories-aef8d367.js
+++
@@ -1,4 +0,0 @@
-import{D as s}from"./Divider-782d73db.js";import"./jsx-runtime-69eee039.js";import"./index-7c191284.js";import"./Box-8ffdd394.js";/* empty css                            */import"./atoms-c7901a03.js";const d={title:"Components/Divider",component:s},r={args:{}};var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {}
-}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,d as default};
-//# sourceMappingURL=Divider.stories-aef8d367.js.map
