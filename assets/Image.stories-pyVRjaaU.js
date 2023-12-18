2c7bd23e05e822e574d114990ba92f553cf88f04
corbanbrook <corbanbrook@gmail.com>
2023-12-18 22:31:05 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@9d573a63bf81973a6eb744d6379be341e3770045 🚀

---
+++ assets/Image.stories-pyVRjaaU.js
@@ -0,0 +1,5 @@
+import{I as s}from"./Image-Lx4Jmnye.js";import"./jsx-runtime-baBBSKBE.js";import"./index-PzSkSLoa.js";import"./Box-3dY9zA3P.js";/* empty css                            */import"./atoms-wgLQZhMV.js";import"./motion-rIg1yyuu.js";const u={title:"Components/Image",component:s},a="https://skyweaver.ghost.io/content/images/size/w600/2023/01/hexbound_promo.png",o={args:{src:a}};var r,e,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    src
+  }
+}`,...(t=(e=o.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const l=["Default"];export{o as Default,l as __namedExportsOrder,u as default};
---
+++ assets/TokenImage.stories-RO-qQAwE.js
@@ -0,0 +1,12 @@
+import{T as c}from"./TokenImage-T1PkOCrq.js";import"./jsx-runtime-baBBSKBE.js";import"./index-PzSkSLoa.js";import"./Box-3dY9zA3P.js";/* empty css                            */import"./atoms-wgLQZhMV.js";import"./Image-Lx4Jmnye.js";import"./motion-rIg1yyuu.js";import"./Text-evKwfSQG.js";import"./typography-kaheFGw9.js";/* empty css                              */import"./createRuntimeFn-f8e161c6.esm-AdCGDnTf.js";const T={title:"Components/TokenImage",component:c},o={args:{borderRadius:"circle",size:"lg",src:"https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912"}},e={args:{size:"lg",symbol:"SCAM"}};var r,s,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    borderRadius: 'circle',
+    size: 'lg',
+    src: 'https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912'
+  }
+}`,...(t=(s=o.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var a,i,m;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    size: 'lg',
+    symbol: 'SCAM'
+  }
+}`,...(m=(i=e.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const W=["WithImage","WithoutImage"];export{o as WithImage,e as WithoutImage,W as __namedExportsOrder,T as default};
