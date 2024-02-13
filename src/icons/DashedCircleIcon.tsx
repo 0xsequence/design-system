42bdc9f3876e93cef159ca700788b98674898283
Ahmet Buğra Yiğiter <yigiterahmetbugra@gmail.com>
2024-02-13 20:01:40 +0300

    Add missing icons for builder (#82)

---
+++ src/icons/DashedCircleIcon.tsx
@@ -0,0 +1,39 @@
+// Auto-generated file created by svgr-cli source /src/icons/template.js
+// Run pnpm build:icons to update
+import { SVGProps } from 'react'
+
+import { Box } from '~/components/Box'
+
+import { iconVariants } from './styles.css'
+import { IconProps } from './types'
+
+const Svg = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 20 20"
+    {...props}
+  >
+    <circle
+      cx={10}
+      cy={10}
+      r={8}
+      stroke="currentColor"
+      strokeDasharray="2 4"
+      strokeLinecap="round"
+      strokeWidth={1.5}
+    />
+  </svg>
+)
+
+const SvgDashedCircleIcon = ({ size = 'sm', ...props }: IconProps) => (
+  <Box
+    as={Svg}
+    className={iconVariants({
+      size,
+    })}
+    {...props}
+  />
+)
+
+export default SvgDashedCircleIcon
