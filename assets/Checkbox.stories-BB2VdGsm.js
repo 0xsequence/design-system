19c7b5656dbd7492a153bc084e93cbbc0e336270
corbanbrook <corbanbrook@gmail.com>
2024-04-09 23:44:07 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@61e9442e9b5dbbd2e825dee82355b40c639fb878 🚀

--- assets/Checkbox.stories-BB2VdGsm.js
+++
@@ -1,8 +0,0 @@
-import{C as e}from"./Checkbox-ro_cnvqA.js";import"./jsx-runtime-DoxjgJx5.js";import"./index-Cu9bd8lq.js";import"./index-BUipU-ak.js";import"./index-Ckls47V4.js";import"./index-BdmuMSao.js";import"./index-BtV2h-_m.js";import"./index-C_rCLuZS.js";import"./index-DMoNf_Sx.js";import"./Field-B5aeH7EX.js";import"./Box-DcptIfsD.js";/* empty css                            */import"./atoms-IWQDe5VI.js";import"./Text-C3GaNfmO.js";import"./typography-BClcJN8w.js";/* empty css                              */import"./createRuntimeFn-f8e161c6.esm-CCZOQpn0.js";import"./WarningIcon-DHWctdlW.js";const D={title:"Forms/Checkbox",component:e},o={args:{label:"This is the label (CheckmarkIcon is missing)",labelLocation:"right",size:"sm",disabled:!1}};var r,i,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    label: 'This is the label (CheckmarkIcon is missing)',
-    labelLocation: 'right',
-    size: 'sm',
-    disabled: false
-  }
-}`,...(t=(i=o.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const I=["Default"];export{o as Default,I as __namedExportsOrder,D as default};
