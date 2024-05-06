e81300ba30c0861e0453c8926d2288d18431b3dd
Corban Riley <corbanbrook@gmail.com>
2024-05-06 13:10:04 -0400

    Fixing reset

--- src/css/reset.css.ts
+++ src/css/reset.css.ts
@@ -2,9 +2,7 @@ import { globalStyle, style } from '@vanilla-extract/css'
 
 import { vars } from './vars.css'
 
-export const reset = style({
-  textDecoration: 'unset',
-})
+export const reset = style({})
 
 globalStyle(`${reset}, ${reset} *`, {
   fontFamily: vars.fonts.body,
