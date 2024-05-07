6809668ab7ddef5940015bd3d5cfe741b15df351
corbanbrook <corbanbrook@gmail.com>
2024-05-07 18:09:47 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@f79272851493f670cb7f4fb024fa3698ad031cae 🚀

---
+++ assets/Checkbox.stories-KNboCMXk.js
@@ -0,0 +1,8 @@
+import{C as e}from"./Checkbox-D-cO98pj.js";import"./jsx-runtime-CexXSJP5.js";import"./index-BP8_t0zE.js";import"./index-CeXBLvNc.js";import"./index-BxmsGmlx.js";import"./index-C-LszQ6e.js";import"./index-CF1bRcOk.js";import"./index-pC7lLcKS.js";import"./index-CnD1ylEj.js";import"./Field-B02RAn6P.js";import"./Box-5Op_64AG.js";import"./atoms-BnxHeJkh.js";/* empty css                            */import"./Text-QGAR2i4c.js";import"./typography-BClcJN8w.js";/* empty css                              */import"./createRuntimeFn-62c9670f.esm-UsP-ae2O.js";import"./WarningIcon-CkKBl9Gd.js";const D={title:"Forms/Checkbox",component:e},o={args:{label:"This is the label (CheckmarkIcon is missing)",labelLocation:"right",size:"sm",disabled:!1}};var r,i,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    label: 'This is the label (CheckmarkIcon is missing)',
+    labelLocation: 'right',
+    size: 'sm',
+    disabled: false
+  }
+}`,...(t=(i=o.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const I=["Default"];export{o as Default,I as __namedExportsOrder,D as default};
---
+++ assets/Switch.stories-DZYH_V4k.js
@@ -0,0 +1,7 @@
+import{S as e}from"./Switch-DLlwtXv5.js";import"./jsx-runtime-CexXSJP5.js";import"./index-BP8_t0zE.js";import"./index-CeXBLvNc.js";import"./index-BxmsGmlx.js";import"./index-C-LszQ6e.js";import"./index-CF1bRcOk.js";import"./index-pC7lLcKS.js";import"./Box-5Op_64AG.js";import"./atoms-BnxHeJkh.js";/* empty css                            */import"./Field-B02RAn6P.js";import"./Text-QGAR2i4c.js";import"./typography-BClcJN8w.js";/* empty css                              */import"./createRuntimeFn-62c9670f.esm-UsP-ae2O.js";const _={title:"Forms/Switch",component:e},t={args:{disabled:!1,label:"Switch Title",description:"Switch Description"}};var r,i,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    disabled: false,
+    label: 'Switch Title',
+    description: 'Switch Description'
+  }
+}`,...(o=(i=t.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const x=["Default"];export{t as Default,x as __namedExportsOrder,_ as default};
