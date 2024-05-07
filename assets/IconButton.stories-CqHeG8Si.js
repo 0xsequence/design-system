3d957c7c9704dda7949d514459d42fdedb5d8676
corbanbrook <corbanbrook@gmail.com>
2024-05-07 18:21:04 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@50bcb1d0632bd170cfa92cdf5eef6de538f7aecd 🚀

--- assets/IconButton.stories-CqHeG8Si.js
+++
@@ -1,9 +0,0 @@
-import{a as e}from"./WarningIcon-CkKBl9Gd.js";import{I as s}from"./IconButton-Cuq55_n-.js";import"./jsx-runtime-CexXSJP5.js";import"./index-BP8_t0zE.js";import"./Box-5Op_64AG.js";import"./atoms-BnxHeJkh.js";/* empty css                            */import"./createRuntimeFn-62c9670f.esm-UsP-ae2O.js";import"./Button-Bc_4MlyI.js";import"./Text-QGAR2i4c.js";import"./typography-BClcJN8w.js";/* empty css                              */const v={title:"Components/IconButton",component:s},o={args:{icon:e,disabled:!1,size:"md",onClick:()=>console.log("ouch!"),variant:"raised"}};var r,t,n;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    icon: ArrowRightIcon,
-    disabled: false,
-    size: 'md',
-    onClick: () => console.log('ouch!'),
-    variant: 'raised'
-  }
-}`,...(n=(t=o.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const B=["Default"];export{o as Default,B as __namedExportsOrder,v as default};
