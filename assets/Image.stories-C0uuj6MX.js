4e78aa75b8f7b0bc964f10a212da401ad058856e
corbanbrook <corbanbrook@gmail.com>
2024-05-10 15:23:31 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@84167e0228a8bb1f09fb23adec5e6c7be2a66ced 🚀

--- assets/Image.stories-C0uuj6MX.js
+++
@@ -1,5 +0,0 @@
-import{I as s}from"./Image-4d21eryY.js";import"./jsx-runtime-CexXSJP5.js";import"./index-BP8_t0zE.js";import"./Box-Cj61Dz8D.js";import"./helpers.css.ts.vanilla-CPoYynXN.js";/* empty css                            */import"./motion-Djta419p.js";const u={title:"Components/Image",component:s},a="https://skyweaver.ghost.io/content/images/size/w600/2023/01/hexbound_promo.png",o={args:{src:a}};var r,e,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    src
-  }
-}`,...(t=(e=o.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const l=["Default"];export{o as Default,l as __namedExportsOrder,u as default};
