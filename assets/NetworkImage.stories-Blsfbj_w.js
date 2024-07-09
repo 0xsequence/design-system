6fa03e04a6d437e51afa8ec8b6ec7271b4111e37
corbanbrook <corbanbrook@gmail.com>
2024-07-09 15:56:21 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@aadf06e745c633d02f305ab04ca94661fac59027 🚀

---
+++ assets/NetworkImage.stories-Blsfbj_w.js
@@ -0,0 +1,6 @@
+import{N as a}from"./NetworkImage-DuvUonq7.js";import"./jsx-runtime-CexXSJP5.js";import"./index-BP8_t0zE.js";import"./Box-DBNdP3Vb.js";import"./helpers.css.ts.vanilla-R394IIDY.js";/* empty css                            */import"./Image-CRnvJI4R.js";import"./motion-Djta419p.js";import"./createRuntimeFn-62c9670f.esm-UsP-ae2O.js";const l={title:"Components/NetworkImage",component:a},r={args:{size:"lg",chainId:1}};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    size: 'lg',
+    chainId: 1
+  }
+}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const h=["WithImage"];export{r as WithImage,h as __namedExportsOrder,l as default};
