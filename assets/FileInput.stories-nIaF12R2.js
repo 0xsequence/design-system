cad4bce76042470bc229de05b8bb21932f98c701
corbanbrook <corbanbrook@gmail.com>
2024-09-23 15:14:54 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@7032b5be88cdd1a7c27225c566043d426f7076d9 🚀

---
+++ assets/FileInput.stories-nIaF12R2.js
@@ -0,0 +1,10 @@
+import{F as n}from"./FileInput-CAX8OMui.js";import"./jsx-runtime-CexXSJP5.js";import"./index-BP8_t0zE.js";import"./Box-DJLMDG3D.js";import"./helpers.css.ts.vanilla-9D4iT2FY.js";/* empty css                            */import"./Field-C5nQ_vHE.js";import"./Text-Dnm_bV7o.js";import"./typography-BClcJN8w.js";/* empty css                              */import"./createRuntimeFn-62c9670f.esm-UsP-ae2O.js";import"./IconButton-BrbpAdvv.js";import"./Button-WvtjVgFF.js";import"./WarningIcon-YP0d-6T1.js";const E={title:"Forms/FileInput",component:n},o={args:{borderRadius:"md",name:"myFileInput",label:"File input",labelLocation:"top",validExtensions:["images"],onValueChange:r=>console.log(r)}};var e,t,i;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    borderRadius: 'md',
+    name: 'myFileInput',
+    label: 'File input',
+    labelLocation: 'top',
+    validExtensions: ['images'],
+    onValueChange: file => console.log(file)
+  }
+}`,...(i=(t=o.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const _=["Default"];export{o as Default,_ as __namedExportsOrder,E as default};
