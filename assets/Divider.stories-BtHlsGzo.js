56974b295f6fc6c504966ff8e303a92efe3e5bd5
corbanbrook <corbanbrook@gmail.com>
2024-09-26 19:12:07 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@37789c5cac3beeca2b5c4b22ea59339c83ce68a2 🚀

---
+++ assets/Divider.stories-BtHlsGzo.js
@@ -0,0 +1,3 @@
+import{D as s}from"./Divider-C4wmo-LC.js";import"./jsx-runtime-CexXSJP5.js";import"./index-BP8_t0zE.js";import"./Box-BiOOL6Xk.js";import"./helpers.css.ts.vanilla-C3xvljdQ.js";/* empty css                            */const d={title:"Components/Divider",component:s},r={args:{}};var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {}
+}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,d as default};
