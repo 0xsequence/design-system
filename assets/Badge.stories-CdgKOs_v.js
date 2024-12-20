f9c7088b5d9d734b50762f759f6a9047f12811b7
corbanbrook <corbanbrook@gmail.com>
2024-12-20 16:28:45 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@84eac93b129852fcbf52dc6af12580493b8dbd71 🚀

---
+++ assets/Badge.stories-CdgKOs_v.js
@@ -0,0 +1,11 @@
+import{B as i}from"./Badge-BLTH9L2x.js";import"./jsx-runtime-CexXSJP5.js";import"./index-BP8_t0zE.js";import"./Box-DzhspURE.js";import"./helpers.css.ts.vanilla-C3xvljdQ.js";/* empty css                            *//* empty css                              */import"./createRuntimeFn-62c9670f.esm-UsP-ae2O.js";const f={title:"Components/Badge",component:i},r={args:{value:"3",variant:"info"}},a={args:{value:"2713",variant:"success"}};var e,s,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    value: '3',
+    variant: 'info'
+  }
+}`,...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var o,n,c;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    value: '2713',
+    variant: 'success'
+  }
+}`,...(c=(n=a.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const h=["SingleCharacter","MultiCharacter"];export{a as MultiCharacter,r as SingleCharacter,h as __namedExportsOrder,f as default};
