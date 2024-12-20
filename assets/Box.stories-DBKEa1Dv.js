2337775d40cd737e9dd9de42e6457804c191da79
corbanbrook <corbanbrook@gmail.com>
2024-12-20 16:38:39 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@79405afabdc8b502c0193804142b305eeee1d7cd 🚀

---
+++ assets/Box.stories-DBKEa1Dv.js
@@ -0,0 +1,9 @@
+import{t as a}from"./index-DMJMdeiN.js";import{B as n}from"./Box-DzhspURE.js";import"./color-D_Ox65gw.js";import"./typography-BClcJN8w.js";import"./index-BP8_t0zE.js";import"./helpers.css.ts.vanilla-C3xvljdQ.js";/* empty css                            */const u={title:"Components/Box",component:n,argTypes:{padding:{control:"select",options:Object.keys(a.space)}}},o={args:{padding:"4",background:"backgroundSecondary",borderRadius:"md",color:"text100",children:"Hello"}};var r,e,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    padding: '4',
+    background: 'backgroundSecondary',
+    borderRadius: 'md',
+    color: 'text100',
+    children: 'Hello'
+  }
+}`,...(t=(e=o.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const g=["Default"];export{o as Default,g as __namedExportsOrder,u as default};
--- assets/IconButton.stories-Bf7VMLl2.js
+++
@@ -1,9 +0,0 @@
-import{a as e}from"./WarningIcon-mCypRAIk.js";import{I as s}from"./IconButton-p0ntQV34.js";import"./jsx-runtime-CexXSJP5.js";import"./index-BP8_t0zE.js";import"./Box-BTyrg29j.js";import"./helpers.css.ts.vanilla-C3xvljdQ.js";/* empty css                            */import"./createRuntimeFn-62c9670f.esm-UsP-ae2O.js";import"./Button-DnRzWzGv.js";import"./Text-DVJSgq1G.js";import"./typography-BClcJN8w.js";/* empty css                              */const v={title:"Components/IconButton",component:s},o={args:{icon:e,disabled:!1,size:"md",onClick:()=>console.log("ouch!"),variant:"raised"}};var r,t,n;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    icon: ArrowRightIcon,
-    disabled: false,
-    size: 'md',
-    onClick: () => console.log('ouch!'),
-    variant: 'raised'
-  }
-}`,...(n=(t=o.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const B=["Default"];export{o as Default,B as __namedExportsOrder,v as default};
