9ea078c0c646671b056380186de820bb8b3d40cd
Corban Riley <corbanbrook@gmail.com>
2025-01-07 15:46:16 -0500

    Image converted to tailwind

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,14 +1,12 @@
-import { motion } from 'framer-motion'
+import { motion, HTMLMotionProps } from 'framer-motion'
 import { SyntheticEvent, useState } from 'react'
 
-import { Box, BoxProps } from '../Box'
-
-interface ImageProps extends BoxProps<typeof motion.img> {
+interface ImageProps extends HTMLMotionProps<'img'> {
   disableAnimation?: boolean
 }
 
 export const Image = (props: ImageProps) => {
-  const { disableAnimation = false, onLoad, ...rest } = props
+  const { disableAnimation = false, onLoad, className, ...rest } = props
   const [isImageLoaded, setImageLoaded] = useState(false)
 
   const handleLoad = (ev: SyntheticEvent<HTMLImageElement, Event>) => {
@@ -17,8 +15,8 @@ export const Image = (props: ImageProps) => {
   }
 
   return (
-    <Box
-      as={motion.img}
+    <motion.img
+      className={className}
       initial={disableAnimation ? undefined : { opacity: 0 }}
       animate={
         disableAnimation ? undefined : { opacity: isImageLoaded ? 1 : 0 }
