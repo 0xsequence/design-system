32e935032732444e4d807cb8caf06abf43e5f742
Corban Riley <corbanbrook@gmail.com>
2025-01-23 11:30:26 -0500

    Migrating Storybook from v7->v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,19 @@
-export default {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+import { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+
   staticDirs: ['../public'],
-  // docs: {
-  //  docsPage: 'automatic'
-  // }
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript', // Offers better detection of prop types
+  },
 }
+
+export default config
