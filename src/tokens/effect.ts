96e75649e1f37a17b042f753d205586d9c34072b
Corban Riley <corbanbrook@gmail.com>
2025-01-30 15:40:55 -0500

    Removing unused tokens

--- src/tokens/effect.ts
+++
@@ -1,4 +0,0 @@
-export const blur = {
-  blur: 'blur(12.5px)',
-  none: 'none',
-}
--- src/tokens/index.ts
+++ src/tokens/index.ts
@@ -1,15 +1,4 @@
-import { colors } from './color'
-import { blur } from './effect'
-import { fonts } from './typography'
-
-export const tokens = {
-  blur,
-  colors,
-  fonts,
-}
-
 export { breakpoints } from './breakpoints'
 export { colors } from './color'
 
 export type { ColorScheme } from './color'
-export type Tokens = typeof tokens
