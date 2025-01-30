8143e823005dc6eec93aecad094f8824dc7f138d
Corban Riley <corbanbrook@gmail.com>
2025-01-30 11:48:02 -0500

    Removing unused tokens

--- src/tokens/index.ts
+++ src/tokens/index.ts
@@ -1,30 +1,11 @@
-import { borderWidths, radii } from './border'
 import { colors } from './color'
 import { blur } from './effect'
-import { opacity } from './opacity'
-import { space } from './space'
-import {
-  fonts,
-  fontSizes,
-  fontWeights,
-  letterSpacings,
-  lineHeights,
-} from './typography'
-import { zIndex } from './z-index'
+import { fonts } from './typography'
 
 export const tokens = {
   blur,
-  borderWidths,
   colors,
   fonts,
-  fontSizes,
-  fontWeights,
-  letterSpacings,
-  lineHeights,
-  opacity,
-  radii,
-  space,
-  zIndex,
 }
 
 export { breakpoints } from './breakpoints'
