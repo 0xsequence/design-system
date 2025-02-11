0330664df016b38f89b79ea08b60ee8357e457ae
corbanbrook <corbanbrook@gmail.com>
2025-02-11 23:49:26 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@3ff319a801f69a53537d905abbadbfadab5a2ae8 🚀

---
+++ assets/Icon.stories-ilsInmsN.js
@@ -0,0 +1,6 @@
+import{d as o}from"./WarningIcon-BDb1uTA1.js";import"./jsx-runtime-z8MfsBtr.js";import"./index-C9rmetsa.js";import"./classnames-D6E8UiG_.js";import"./clsx-B-dksMZM.js";import"./index-DKHC-OhD.js";const d={title:"Components/Icons",component:o},r={args:{className:"text-primary",size:"md"}};var t,e,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    className: 'text-primary',
+    size: 'md'
+  }
+}`,...(s=(e=r.parameters)==null?void 0:e.docs)==null?void 0:s.source}}};const l=["Default"];export{r as Default,l as __namedExportsOrder,d as default};
