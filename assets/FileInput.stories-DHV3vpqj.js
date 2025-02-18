3b287d5a6f32bdadf3ed5e53cb63ea517f8da20c
corbanbrook <corbanbrook@gmail.com>
2025-02-18 14:18:46 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@ebeb6fa4249c40b986fd8d20b2d8308be48579a2 🚀

---
+++ assets/FileInput.stories-DHV3vpqj.js
@@ -0,0 +1,9 @@
+import{F as r}from"./FileInput-CPR-19kH.js";import"./jsx-runtime-z8MfsBtr.js";import"./index-C9rmetsa.js";import"./index-D1TSixAU.js";import"./Field-DFoEKqgL.js";import"./index-DKHC-OhD.js";import"./clsx-B-dksMZM.js";import"./Text-BQ_5oice.js";import"./index-DEvaI7vP.js";import"./classnames-D6E8UiG_.js";import"./IconButton-BV-qU8MD.js";import"./Button-CAqaY53K.js";import"./WarningIcon-BDb1uTA1.js";const x={title:"Forms/FileInput",component:r},o={args:{name:"myFileInput",label:"File input",labelLocation:"top",validExtensions:["images"],onValueChange:n=>console.log(n)}};var e,t,i;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    name: 'myFileInput',
+    label: 'File input',
+    labelLocation: 'top',
+    validExtensions: ['images'],
+    onValueChange: file => console.log(file)
+  }
+}`,...(i=(t=o.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const E=["Default"];export{o as Default,E as __namedExportsOrder,x as default};
--- assets/NetworkImage.stories-Cpyx98N5.js
+++
@@ -1,6 +0,0 @@
-import{N as a}from"./NetworkImage-CZ8OPR-h.js";import"./jsx-runtime-z8MfsBtr.js";import"./index-C9rmetsa.js";import"./index-DKHC-OhD.js";import"./clsx-B-dksMZM.js";import"./index-D1TSixAU.js";import"./classnames-D6E8UiG_.js";import"./Image-DOwPja2B.js";import"./proxy-DNmaJl8h.js";const l={title:"Components/NetworkImage",component:a},r={args:{size:"lg",chainId:1}};var o,e,t;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    size: 'lg',
-    chainId: 1
-  }
-}`,...(t=(e=r.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const h=["WithImage"];export{r as WithImage,h as __namedExportsOrder,l as default};
