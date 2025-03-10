2fb903c91ce2c7d741fb27e43a05566516990ab3
Mithat Akbulut <marmarisco860@gmail.com>
2025-03-10 20:50:45 +0300

    add svgs and run pnpm build:icons

---
+++ src/icons/Twitter.tsx
@@ -0,0 +1,36 @@
+// Auto-generated file created by svgr-cli source /src/icons/template.js
+// Run pnpm build:icons to update
+import { SVGProps } from 'react'
+
+import { cn } from '~/utils'
+
+import { iconVariants } from './iconVariants'
+import { IconProps } from './types'
+
+const Svg = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 20 20"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M11.523 8.776 17.482 2H16.07l-5.174 5.883L6.766 2H2l6.247 8.896L2 18h1.411l5.461-6.214L13.234 18H18M3.92 3.04h2.169l9.979 13.971H13.9"
+    />
+  </svg>
+)
+
+const SvgTwitter = ({ className, size = 'sm', ...props }: IconProps) => (
+  <Svg
+    className={cn(
+      iconVariants({
+        size,
+      }),
+      className
+    )}
+    {...props}
+  />
+)
+
+export default SvgTwitter
