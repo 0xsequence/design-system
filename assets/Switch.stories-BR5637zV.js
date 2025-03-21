94d9b45540c3b1bea7894fbc956724dd606f8e74
corbanbrook <corbanbrook@gmail.com>
2025-03-21 15:40:56 +0000

    Deploying to gh-pages from @ 0xsequence/design-system@1661b04acef6828355c4a54b302ef64064f49d9f 🚀

---
+++ assets/Switch.stories-BR5637zV.js
@@ -0,0 +1,7 @@
+import{S as e}from"./Switch-SndtdOef.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-CFahbR6w.js";import"./index-By4xKCrq.js";import"./index-AemB5ySP.js";import"./index-uKUYdOYu.js";import"./index-BHL8jL0s.js";import"./index-chp1rBSu.js";import"./index-BLmjmuzb.js";import"./index-bEydcn_X.js";import"./Field-uTSufkFG.js";import"./index-Dp3B9jqt.js";import"./clsx-B-dksMZM.js";import"./Text-Vf0bTgHv.js";import"./classnames-Dx2cly0x.js";const g={title:"Forms/Switch",component:e},t={args:{disabled:!1,label:"Switch Title",description:"Switch Description"}};var r,i,o;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    disabled: false,
+    label: 'Switch Title',
+    description: 'Switch Description'
+  }
+}`,...(o=(i=t.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const _=["Default"];export{t as Default,_ as __namedExportsOrder,g as default};
---
+++ assets/index-BitAuI66.js
@@ -0,0 +1,1 @@
+import"./preview-DnZlQTI5.js";import"./DocsRenderer-CFRXHY34-DsCVvFzf.js";const{definePreview:i}=__STORYBOOK_MODULE_PREVIEW_API__;
