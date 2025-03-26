8a1edc52a4811a665f77c1d701a4f55c99e4f28a
Corban Riley <corbanbrook@gmail.com>
2025-03-26 17:00:57 -0400

    Fixing duplicated icons

--- src/icons/PauseIcon.tsx
+++ src/icons/PauseIcon.tsx
@@ -11,13 +11,13 @@ const Svg = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
-    viewBox="0 0 25 24"
+    viewBox="0 0 24 24"
     {...props}
   >
     <path
       fill="currentColor"
       fillRule="evenodd"
-      d="M4 5a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm5 0H6v14h3zM14 5a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0h-3v14h3z"
+      d="M3.5 5a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0h-3v14h3zM13.5 5a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0h-3v14h3z"
       clipRule="evenodd"
     />
   </svg>
