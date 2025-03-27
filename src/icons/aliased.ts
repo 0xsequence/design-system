b35ba5a422793e53360125828d7fa2f7de607f80
Corban Riley <corbanbrook@gmail.com>
2025-03-27 10:49:01 -0400

    Adding icon aliases for common icons which were renamed

---
+++ src/icons/aliased.ts
@@ -0,0 +1,4 @@
+export { default as SunIcon } from './LightModeIcon'
+export { default as MoonIcon } from './DarkModeIcon'
+export { default as EllipsisIcon } from './ContextMenuIcon'
+export { default as ReceiveIcon } from './DownloadIcon'
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * as tokens from '~/tokens'
 
 export * from '~/components'
 export * from '~/icons'
+export * from '~/icons/aliased'
 export type { Icon, IconProps } from '~/icons/types'
 
 export * from '~/hooks'
