bf46b2e676f2928df6f81db2635f06b660e141e8
Corban Riley <corbanbrook@gmail.com>
2025-05-01 13:48:01 -0400

    Fixing Divider story typing

--- src/components/Divider/Divider.stories.tsx
+++ src/components/Divider/Divider.stories.tsx
@@ -1,11 +1,11 @@
-import type { StoryObj } from '@storybook/react'
+import type { StoryObj, Meta } from '@storybook/react'
 
 import { Divider } from './Divider.js'
 
 export default {
   title: 'Components/Divider',
   component: Divider,
-}
+} as Meta<typeof Divider>
 
 type Story = StoryObj<typeof Divider>
 
