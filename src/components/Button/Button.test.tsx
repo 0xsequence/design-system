75f4d2aacbeb4af0cf1518c5d763ded31ceb3355
Corban Riley <corbanbrook@gmail.com>
2025-05-01 13:05:26 -0400

    Fixing userEvent setup() type error

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,5 +1,5 @@
 import { cleanup, render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { userEvent } from '@testing-library/user-event'
 import { vi } from 'vitest'
 
 import { Button } from './Button.js'
--- src/components/Tooltip/Tooltip.test.tsx
+++ src/components/Tooltip/Tooltip.test.tsx
@@ -1,5 +1,5 @@
 import { cleanup, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { userEvent } from '@testing-library/user-event'
 
 import { renderWithTheme } from '~/components/ThemeProvider/renderWithTheme.js'
 
