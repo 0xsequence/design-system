675d3c128913a581a4b480be866491e2d7898b26
Corban Riley <corbanbrook@gmail.com>
2025-10-05 14:13:39 -0400

    Adding tw-animate-css

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -14,6 +14,7 @@ export const PopoverContent = (props: PopoverPrimitive.PopoverContentProps) => {
       <PopoverPrimitive.Content
         className={cn(
           'shadow-primary rounded-2xl bg-background-primary border-1 border-border-normal',
+          'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2',
           className
         )}
         {...rest}
