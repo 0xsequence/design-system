4c6c40435def91f316e3c2db58f6343fe05116c3
Corban Riley <corbanbrook@gmail.com>
2025-10-23 08:59:43 -0400

    Apply a default size to icons in w and h terms rather than size so we know if a size is specified

--- src/icons/iconVariants.ts
+++ src/icons/iconVariants.ts
@@ -1,8 +1,9 @@
 import { cva } from 'class-variance-authority'
 
-export const iconVariants = cva('block shrink-0 w-5 h-5', {
+export const iconVariants = cva('block shrink-0', {
   variants: {
     size: {
+      default: 'w-5 h-5', // Don't apply a size class when no size is specified
       xxs: 'size-3',
       xs: 'size-4',
       sm: 'size-5',
@@ -10,5 +11,8 @@ export const iconVariants = cva('block shrink-0 w-5 h-5', {
       lg: 'size-7',
       xl: 'size-9',
     },
+  },
+  defaultVariants: {
+    size: 'default',
   },
 })
