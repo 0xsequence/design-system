854f5e3a0d2afa7102d0208fbd254a68b9dd76b5
Corban Riley <criley@polygon.technology>
2026-03-12 23:08:59 -0400

    Fixing Toast animation

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,6 +1,6 @@
 import {
-    Toast as ToastPrimitive,
-    type ToastManagerAddOptions,
+  Toast as ToastPrimitive,
+  type ToastManagerAddOptions,
 } from '@base-ui/react/toast'
 import { clsx } from 'clsx'
 import { type ComponentType } from 'react'
@@ -37,7 +37,6 @@ function ToastList() {
 
   return (
     <ToastPrimitive.Viewport className="fixed z-1 w-[250px] bottom-4 right-4 left-auto top-auto mx-auto sm:bottom-8 sm:right-8 sm:w-[300px] focus:outline-hidden">
-      {/* <ToastPrimitive.Viewport className="fixed right-0 bottom-0 focus:outline-hidden w-full flex flex-col gap-2 p-4 pt-0 z-1000 list-none max-w-[532px]"> */}
       {toasts.map(toast => (
         <Toast key={toast.id} toast={toast} />
       ))}
@@ -64,19 +63,41 @@ function Toast({
         '[--shrink:calc(1-var(--scale))]',
         '[--height:var(--toast-frontmost-height,var(--toast-height))]',
         '[--offset-y:calc(var(--toast-offset-y)*-1+(var(--toast-index)*var(--gap)*-1)+var(--toast-swipe-movement-y))]',
-        'absolute right-0 bottom-0 left-auto mx-auto mr-0 origin-bottom select-none',
-        'transition-[transform,opacity,height] duration-500 ease-[cubic-bezier(0.22,1,0.36,1)]',
+      
+        // layout / base
+        'absolute right-0 bottom-0 left-auto mx-auto mr-0',
+        'origin-bottom select-none',
+        'cursor-default',
+      
+        // transitions & stacking
+        '[transition:transform_0.5s_cubic-bezier(0.22,1,0.36,1),opacity_0.5s,height_0.15s]',
         'z-[calc(1000-var(--toast-index))]',
+      
+        // height & default transform (matches your "transform: translateX(...) translateY(...) scale(...)")
         'h-(--height)',
-        'transform-[translateX(var(--toast-swipe-movement-x))_translateY(calc(var(--toast-swipe-movement-y)-(var(--toast-index)*var(--peek))-(var(--shrink)*var(--height))))_scale(var(--scale))]',
-        'data-expanded:transform-[translateX(var(--toast-swipe-movement-x))_translateY(var(--offset-y))] data-expanded:h-(--toast-height)',
-        'data-starting-style:translate-y-[150%] data-ending-style:translate-y-[150%]',
-        'data-limited:opacity-0',
+        "transform-[translateX(var(--toast-swipe-movement-x))_translateY(calc(var(--toast-swipe-movement-y)-(var(--toast-index)*var(--peek))-(var(--shrink)*var(--height))))_scale(var(--scale))]",
+      
+        // expanded state: move to offset-y and set height
+        'data-expanded:transform-[translateX(var(--toast-swipe-movement-x))_translateY(var(--offset-y))]',
+        'data-expanded:h-(--toast-height)',
+      
+        // starting / ending off-screen
+        'data-starting-style:transform-[translateY(150%)]!',
+        'data-ending-style:transform-[translateY(150%)]!',
+      
+        // ending style 
         'data-ending-style:opacity-0',
-        'data-ending-style:data-[swipe-direction=up]:translate-y-[calc(var(--toast-swipe-movement-y)-150%)]',
-        'data-ending-style:data-[swipe-direction=left]:transform-[translateX(calc(var(--toast-swipe-movement-x)-150%))_translateY(var(--offset-y))]',
-        'data-ending-style:data-[swipe-direction=right]:transform-[translateX(calc(var(--toast-swipe-movement-x)+150%))_translateY(var(--offset-y))]',
-        'data-ending-style:data-[swipe-direction=down]:translate-y-[calc(var(--toast-swipe-movement-y)+150%)]',
+
+        // limited opacity
+        'data-limited:opacity-0',
+
+        // swipe-direction specifics
+        "data-ending-style:data-[swipe-direction=up]:transform-[translateY(calc(var(--toast-swipe-movement-y)-150%))]",
+        "data-ending-style:data-[swipe-direction=left]:transform-[translateX(calc(var(--toast-swipe-movement-x)-150%))_translateY(var(--offset-y))]",
+        "data-ending-style:data-[swipe-direction=right]:transform-[translateX(calc(var(--toast-swipe-movement-x)+150%))_translateY(var(--offset-y))]",
+        "data-ending-style:data-[swipe-direction=down]:transform-[translateY(calc(var(--toast-swipe-movement-y)+150%))]",
+      
+        // pseudo spacer (after)
         "after:content-[''] after:absolute after:top-full after:left-0 after:w-full after:h-[calc(var(--gap)+1px)]",
       ])}
       render={
