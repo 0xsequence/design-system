6838e59111a61fc8df29ba485aff43260db2511a
Corban Riley <criley@polygon.technology>
2026-03-13 15:16:42 -0400

    Fixing Alert font

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -76,7 +76,7 @@ function AlertDescription({ className, ...props }: ComponentProps<'div'>) {
     <div
       data-slot="alert-description"
       className={cn(
-        'text-sm font-medium',
+        'text-sm',
         'text-current col-start-2 col-end-4 grid justify-items-start gap-1 sm:col-end-3',
         className
       )}
