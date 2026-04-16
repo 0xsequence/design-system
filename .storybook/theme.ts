e81eb91256a81defc2483effb8e8548f09ca4f97
Corban Riley <criley@polygon.technology>
2026-04-16 16:53:35 -0400

    Adding polygon branding to Storybook

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -3,4 +3,7 @@ import { create } from 'storybook/theming'
 export default create({
   base: 'light',
   fontBase: 'Fustat',
+  brandUrl: 'https://polygon.technology',
+  brandTitle: 'Polygon / Design System',
+  brandImage: '/static/images/polygon-logo.svg',
 })
